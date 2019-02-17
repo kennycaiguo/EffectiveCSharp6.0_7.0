8657747b469fc4da46c982bd6e073226f6594c51
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 13:55:42 +0900

    item02: ConstReadonly

---
+++ ch01/item02/ConstReadonly/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstReadonly
+{
+    class Program
+    {
+        public const int Millennium = 2000;
+        public static readonly int ThisYear = 2004;
+
+        //private const DateTime classCreation = new DateTime(2000, 1, 1, 0, 0, 0);
+        private static readonly DateTime classCreation = new DateTime(2000, 1, 1, 0, 0, 0);
+
+        static void Main(string[] args)
+        {
+            var myDateTime = new DateTime(2000, 1, 1, 0, 0, 0);
+            if (myDateTime.Year == Millennium)
+            {
+                Console.WriteLine("MyDateTime.Year is Millemminum");
+            }
+
+            Console.WriteLine("classCreation: " + classCreation);
+        }
+    }
+}
