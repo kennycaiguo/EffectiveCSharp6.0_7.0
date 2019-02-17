8d3eaf100eb165afcbf4d22237f3c3233f8c06a0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 14:14:47 +0900

    item02: Infrastrucure/Application

---
+++ ch01/item02/Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastrucure; // 2019.02.17 add
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = UsefulValues.StartValue;
+                i < UsefulValues.EndValue; i++)
+                Console.WriteLine("値は {0}", i);
+        }
+    }
+}
---
+++ ch01/item02/Infrastrucure/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastrucure
+{
+    public class UsefulValues
+    {
+        public static readonly int StartValue = 105;
+        public const int EndValue = 120;
+    }
+}
