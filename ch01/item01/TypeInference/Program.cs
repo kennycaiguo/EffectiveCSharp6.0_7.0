2df459ee74f98295d6eab1244eceefc1df5c776b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 09:25:55 +0900

    item01: TypeInference: factory

--- ch01/item01/TypeInference/Program.cs
+++ ch01/item01/TypeInference/Program.cs
@@ -10,12 +10,26 @@ namespace TypeInference
     {
 
     }
+    class Account
+    {
+
+    }
+    class AccountFactory
+    {
+        public static Account CreateSavingsAccount()
+        {
+            return new Account();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             var foo = new MyType();
             Console.WriteLine($"宣言された型：{foo.GetType().Name}");
+
+            var thing = AccountFactory.CreateSavingsAccount();
+            Console.WriteLine($"宣言された型：{thing.GetType().Name}");
         }
     }
 }
