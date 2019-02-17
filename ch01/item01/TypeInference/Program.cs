2860b285c1d5b055dcb271ef82c488cb152a5302
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 09:19:18 +0900

    item01: TypeInference: first commit

---
+++ ch01/item01/TypeInference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeInference
+{
+    class MyType
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new MyType();
+            Console.WriteLine($"宣言された型：{foo.GetType().Name}");
+        }
+    }
+}
