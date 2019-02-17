92517a1db2d2e6766b3ff49fc0a3666e8e9e02a1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 15:12:20 +0900

    item03: As

---
+++ ch01/item03/As/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace As
+{
+    class MyType
+    {
+
+    }
+    class MyType2
+    {
+
+    }
+    class Factory
+    {
+        public static object GetObject()
+        {
+            //return new MyType();
+            return new MyType2();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = Factory.GetObject();
+            MyType t = o as MyType;
+            if (t != null)
+            {
+                Console.WriteLine("o is MyType");
+            }
+            else
+            {
+                Console.WriteLine("o is not MyType");
+            }
+        }
+    }
+}
