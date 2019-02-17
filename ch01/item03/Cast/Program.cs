b220e07a8c7b852133284671e172c5420e086a43
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-17 15:18:45 +0900

    item03: Cast

---
+++ ch01/item03/Cast/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cast
+{
+    class MyType
+    {
+
+    }
+    class MyType2
+    {
+
+    }
+    class Factory
+    {
+        public static object GetObject()
+        {
+            //return new MyType();
+            //return new MyType2();
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = Factory.GetObject();
+            try
+            {
+                MyType t;
+                t = (MyType)o;
+                if (t == null)
+                {
+                    Console.WriteLine("o is null");
+                }
+                else
+                {
+                    Console.WriteLine("o is MyType");
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("o is not MyType");
+            }
+        }
+    }
+}
