619863ebd217365b70c5ff5fa527d9fa975b1fd6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-23 10:52:23 +0900

    item03: AsInt

---
+++ ch01/item03/AsInt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsInt
+{
+    class Factory
+    {
+        public static int GetValue()
+        {
+            return 101;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = Factory.GetValue();
+            // can not compile
+            //int i = o as int;
+            var i = o as int?;
+            if (i != null)
+                Console.WriteLine(i.Value);
+        }
+    }
+}
