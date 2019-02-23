09055beac94c90067bd5a708b9f055649f9d9e0b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-23 19:21:18 +0900

    item06: NullArgument

---
+++ ch01/item06/NullArgument/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullArgument
+{
+    class Program
+    {
+        public static void ExceptionMessage(object thisCantBeNull)
+        {
+            if (thisCantBeNull == null)
+                throw new
+                    ArgumentNullException(nameof(thisCantBeNull),
+                        "値をnullにできません");
+        }
+        static void Main(string[] args)
+        {
+            ExceptionMessage(args);
+            ExceptionMessage(null);
+
+        }
+    }
+}
