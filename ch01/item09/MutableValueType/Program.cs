7e200f94fc620097355575488a0cc7ace08a769b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-24 14:56:03 +0900

    item09: MutableValueType

---
+++ ch01/item09/MutableValueType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutableValueType
+{
+    public struct Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var attendees = new List<Person>();
+            Person p = new Person { Name = "以前の名前" };
+            attendees.Add(p);
+
+            Person p2 = attendees[0];
+            p2.Name = "新しい名前";
+
+            Console.WriteLine(attendees[0].ToString());
+        }
+    }
+}
