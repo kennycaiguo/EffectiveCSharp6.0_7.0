6ac26799271bd06c237d2becf95b2d1d39a4cb92
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-24 11:31:31 +0900

    item08: CallEventHandler

---
+++ ch01/item08/CallEventHandler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallEventHandler
+{
+    public class EventSource
+    {
+        private EventHandler<int> Updated;
+        public void AddHandler(EventHandler<int> h)
+        {
+            Updated += h;
+        }
+        public void ClearHandler()
+        {
+            Updated = null;
+        }
+        public void RaiseUpdates()
+        {
+            counter++;
+            Updated(this, counter);
+        }
+
+        private int counter;
+    }
+    class Program
+    {
+        static void Handler(Object sender, int param)
+        {
+            Console.WriteLine("Handler called: param=" + param);
+        }
+        static void Main(string[] args)
+        {
+            var source = new EventSource();
+            source.AddHandler(Handler);
+            source.RaiseUpdates();
+            source.ClearHandler();
+            source.RaiseUpdates();
+        }
+    }
+}
