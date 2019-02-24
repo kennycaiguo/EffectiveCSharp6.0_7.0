75f8786f0159595f930a1dc0b46de0c4f2758a21
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-24 14:48:24 +0900

    item09: Boxing

---
+++ ch01/item09/Boxing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 1, secondNumber = 2, thirdNumber = 3;
+            Console.WriteLine($"いくつかの数値：{firstNumber}, {secondNumber}, {thirdNumber}");
+            Console.WriteLine($"いくつかの数値：{firstNumber.ToString()}, {secondNumber.ToString()}, {thirdNumber.ToString()}");
+
+        }
+    }
+}
