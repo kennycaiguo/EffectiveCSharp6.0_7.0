a70126fdb2993a14f5ee842b10764cfb820c7c87
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-02 19:42:16 +0900

    item10: NewNonVirtual

---
+++ ch01/item10/NewNonVirtual/NewSubclass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNonVirtual
+{
+    public class MyClass
+    {
+        public void MagicMethod()
+        {
+            Console.WriteLine("MyClass");
+        }
+    }
+
+    public class MyOtherClass : MyClass
+    {
+        // MagicMethodをこのクラス用に再定義
+        public new void MagicMethod()
+        {
+            Console.WriteLine("MyOtherClass");
+        }
+    }
+}
---
+++ ch01/item10/NewNonVirtual/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNonVirtual
+{
+    class Program
+    {
+        static object MakeObject()
+        {
+            return new MyOtherClass();
+        }
+
+        static void Main(string[] args)
+        {
+            object c = MakeObject();
+
+            // MyClass経由で実行
+            MyClass cl = c as MyClass;
+            cl.MagicMethod();
+
+            // MyOtherClass経由で実行
+            MyOtherClass cl2 = c as MyOtherClass;
+            cl2.MagicMethod();
+        }
+    }
+}
