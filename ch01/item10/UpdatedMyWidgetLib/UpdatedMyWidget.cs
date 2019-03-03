b56b63d5232e5a857556a0a6f6db00866c77e180
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-03 13:32:44 +0900

    item10: UpdatedMyWidgetLib, UpdatedMyWidgetClient

---
+++ ch01/item10/UpdatedMyWidgetLib/UpdatedMyWidget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NewBaseWidgetLib;
+
+namespace UpdatedMyWidgetLib
+{
+    public class MyWidget : BaseWidget
+    {
+        public void MyFunction()
+        {
+            Console.WriteLine("MyWidget.MyFunction()");
+            BaseFunction();
+        }
+
+        public void NormalizeAllValues()
+        {
+            Console.WriteLine("MyWidget.NormalizeAllValues(): renamed version");
+
+            // 偶然、親クラスの新しいメソッドが同じ処理を行う場合に限り
+            //　親クラスのメソッドを呼び出す
+            base.NormalizeValues();
+        }
+    }
+}
