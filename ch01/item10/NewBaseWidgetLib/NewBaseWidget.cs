61f61d28847f4f2fb5dd412b5929bcb52b7c3f03
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-03 12:05:47 +0900

    item10: NewBaseWidgetLib, NewMyWidgetLib, NewMyWidgetClient

---
+++ ch01/item10/NewBaseWidgetLib/NewBaseWidget.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewBaseWidgetLib
+{
+    public class BaseWidget
+    {
+        public void BaseFunction()
+        {
+            Console.WriteLine("BaseWidget.BaseFunction()");
+        }
+
+        public void NormalizeValues()
+        {
+            Console.WriteLine("BaseWiget.NormalizeValues()");
+        }
+    }
+}
---
+++ ch01/item10/NewMyWidgetLib/NewMyWidget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NewBaseWidgetLib;
+
+namespace NewMyWidgetLib
+{
+    public class MyWidget : BaseWidget
+    {
+        public void MyFunction()
+        {
+            Console.WriteLine("MyWidget.MyFunction()");
+            BaseFunction();
+        }
+
+        public new void NormalizeValues()
+        {
+            Console.WriteLine("MyWidget.NormalizeValues(): new version");
+        }
+    }
+}
