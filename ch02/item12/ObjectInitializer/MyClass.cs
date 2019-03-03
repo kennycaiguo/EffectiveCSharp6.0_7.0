73358c6a479cf11f58287747a0cbecd5e2eb5ac2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 05:53:05 +0900

    item12: ObjectInitializer

---
+++ ch02/item12/ObjectInitializer/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    public class MyClass
+    {
+        // コレクションの宣言と同時に初期化
+        private List<string> labels = new List<string>();
+
+        public List<string> Labels { get { return labels; } }
+    }
+}
---
+++ ch02/item12/ObjectInitializer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            Console.WriteLine($"Labels: {myClass.Labels}, Labels.Count: {myClass.Labels.Count}");
+        }
+    }
+}
