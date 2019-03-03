4ed5e150cae7d593db319d3a3d71f7f7927393ce
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 06:28:28 +0900

    item12: MultipleInitialize

---
+++ ch02/item12/MultipleInitialize/MyClass2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInitialize
+{
+    public class MyClass2
+    {
+        // コレクションを宣言して初期化
+        private List<string> labels = new List<string>();
+
+        public List<string> Labels { get { return labels; } }
+
+        public MyClass2()
+        {
+        }
+
+        public MyClass2(int size)
+        {
+            labels = new List<string>(size);
+        }
+    }
+}
---
+++ ch02/item12/MultipleInitialize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInitialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass2 val1 = new MyClass2();
+            MyClass2 val2 = new MyClass2(10);
+
+            Console.WriteLine($"default constructor: {val1.Labels.Capacity}, constructor with size: {val2.Labels.Capacity}");
+        }
+    }
+}
