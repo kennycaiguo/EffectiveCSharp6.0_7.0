6d9425008dc4252bd70298befd879651d204d4bf
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-03 19:47:43 +0900

    item11: CSharpFinalizer

---
+++ ch02/item11/CSharpFinalizer/CriticalSection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinalizer
+{
+    class CriticalSection
+    {
+        public CriticalSection()
+        {
+            EnterCriticalSection();
+        }
+
+        ~CriticalSection()
+        {
+            ExitCriticalSection();
+        }
+
+        private void EnterCriticalSection()
+        {
+            Console.WriteLine("CriticalSection.EnterCriticalSection()");
+        }
+
+        private void ExitCriticalSection()
+        {
+            Console.WriteLine("CriticalSection.ExitCriticalSection()");
+        }
+
+    }
+}
---
+++ ch02/item11/CSharpFinalizer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriticalSection s = new CriticalSection();
+        }
+    }
+}
