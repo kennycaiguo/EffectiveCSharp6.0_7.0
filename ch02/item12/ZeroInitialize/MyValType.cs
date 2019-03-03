80b20f81b9ecc509675235c68e12d5a76a77206e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 06:12:51 +0900

    item12: ZeroInitialize

---
+++ ch02/item12/ZeroInitialize/MyValType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroInitialize
+{
+    public struct MyValType
+    {
+        int iVal;
+        double dVal;
+        List<int> listVal;
+
+        public int IntVal { get { return iVal; } }
+        public double DoubleVal { get { return dVal; } }
+        public List<int> ListVal { get { return listVal; } }
+    }
+}
---
+++ ch02/item12/ZeroInitialize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroInitialize
+{
+    class Program
+    {
+        static MyValType myVal1;
+
+        static void Main(string[] args)
+        {
+            // 2019.03.04: 未割当のローカル変数
+            //MyValType myVal1;
+            MyValType myVal2 = new MyValType();
+
+            Console.WriteLine($"myVal1.IntVal: {myVal1.IntVal}, myVal1.DoubleVal: {myVal1.DoubleVal}, myVal1.ListVal: {myVal1.ListVal}");
+            Console.WriteLine($"myVal2.IntVal: {myVal2.IntVal}, myVal2.DoubleVal: {myVal2.DoubleVal}, myVal2.ListVal: {myVal2.ListVal}");
+        }
+    }
+}
