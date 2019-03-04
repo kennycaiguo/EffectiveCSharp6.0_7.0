7b4e1c55921ed9bcabe71e39d92b6c273447f150
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 21:14:24 +0900

    item15: DisposableLocalVariable

---
+++ ch02/item15/DisposableLocalVariable/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DisposableLocalVariable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // 悪い例。描画イベントが起こるたびに同じフォントが生成される
+            using (Font MyFont = new Font("Arial", 10.0f))
+            {
+                e.Graphics.DrawString(DateTime.Now.ToString(),
+                    MyFont, Brushes.Black, new Point(0, 0));
+            }
+            base.OnPaint(e);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+}
+
