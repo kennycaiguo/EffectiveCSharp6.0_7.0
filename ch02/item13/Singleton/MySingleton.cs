df6eb0cf9f5b976e8018d55b950f47373121a015
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 11:48:36 +0900

    item13: Singleton

---
+++ ch02/item13/Singleton/MySingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class MySingleton
+    {
+        private static readonly MySingleton theOneAndOnly = new MySingleton();
+
+        public static MySingleton TheOnly
+        {
+            get { return theOneAndOnly; }
+        }
+
+        private MySingleton()
+        {
+            Console.WriteLine("create MySingleton instance");
+        }
+    }
+
+
+}
---
+++ ch02/item13/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = MySingleton.TheOnly;
+            var singleton2 = MySingleton.TheOnly;
+        }
+    }
+}
