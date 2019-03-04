0eb89d8d942664ae1f7b6c0803fbf702132ba6d4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 11:58:57 +0900

    item13: StaticConstructor

---
+++ ch02/item13/StaticConstructor/MySingleton2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstructor
+{
+    public class MySingleton2
+    {
+        private static readonly MySingleton2 theOneAndOnly;
+
+        static MySingleton2()
+        {
+            theOneAndOnly = new MySingleton2();
+        }
+
+        public static MySingleton2 TheOnly
+        {
+            get { return theOneAndOnly; }
+        }
+
+        private MySingleton2()
+        {
+            Console.WriteLine("create MySingleton2 instance");
+        }
+    }
+}
