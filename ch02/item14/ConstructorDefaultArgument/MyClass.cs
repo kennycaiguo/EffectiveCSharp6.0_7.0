b17d14518f5238d92e2e964fcb2e1f4ff8494872
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 16:26:00 +0900

    item14: ConstructorDefaultArgument

---
+++ ch02/item14/ConstructorDefaultArgument/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDefaultArgument
+{
+    public struct ImportantData
+    {
+
+    }
+
+    public class MyClass
+    {
+        // コレクションデータ
+        private List<ImportantData> col1;
+
+        // インスタンス名
+        private string name;
+
+        public List<ImportantData> Collection { get { return col1; } }
+        public string Name { get { return name; } }
+
+        public MyClass() :
+            this(0, string.Empty)
+        {
+        }
+
+        public MyClass(int initialCount = 0, string name = "")
+        {
+            col1 = (initialCount > 0) ?
+                new List<ImportantData>(initialCount) :
+                new List<ImportantData>();
+            this.name = name;
+        }
+    }
+}
---
+++ ch02/item14/ConstructorDefaultArgument/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDefaultArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var var1 = new MyClass();
+            var var2 = new MyClass(10);
+            var var3 = new MyClass(100, "Sophia");
+
+            Console.WriteLine($"var1.Name:{var1.Name}, var1.Collection.Capacity:{var1.Collection.Capacity}");
+            Console.WriteLine($"var2.Name:{var2.Name}, var2.Collection.Capacity:{var2.Collection.Capacity}");
+            Console.WriteLine($"var3.Name:{var3.Name}, var3.Collection.Capacity:{var3.Collection.Capacity}");
+
+            // new制約を確認
+            var list = new List<MyClass> { new MyClass(10), new MyClass(100, "Sophia") };
+            foreach (var item in list)
+            {
+                Console.WriteLine($"Name:{item.Name}, Collection.Capacity:{item.Collection.Capacity}");
+            }
+        }
+    }
+}
