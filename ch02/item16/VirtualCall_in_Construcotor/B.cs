04ff1d303c9c08773417e36d0966f54d0062f45c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 06:28:37 +0900

    item16: VirtualCall_in_Constructor

---
+++ ch02/item16/VirtualCall_in_Construcotor/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualCall_in_Construcotor
+{
+    class B
+    {
+        protected B()
+        {
+            VFunc();
+        }
+
+        protected virtual void VFunc()
+        {
+            Console.WriteLine("B内のVFunc");
+        }
+    }
+}
