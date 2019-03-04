ac59b6875a821cba5d47929f4650940b110105f8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 12:12:14 +0900

    item13: StaticInitializerException

---
+++ ch02/item13/StaticInitializerException/MySingleton2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticInitializerException
+{
+    public class MySingleton2
+    {
+        private static readonly MySingleton2 theOneAndOnly;
+
+        static MySingleton2()
+        {
+            try
+            {
+                theOneAndOnly = new MySingleton2();
+                throw new Exception("Exception in static constructor");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static MySingleton2 TheOnly
+        {
+            get { return theOneAndOnly; }
+        }
+
+        private MySingleton2()
+        {
+            Console.WriteLine("create MySingleton2 instance");
+        }
+    }
+}
---
+++ ch02/item13/StaticInitializerException/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticInitializerException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var var1 = MySingleton2.TheOnly;
+            var var2 = MySingleton2.TheOnly;
+
+            Console.WriteLine($"var1.Equals(var2) ? {var1.Equals(var2)}");
+        }
+    }
+}
