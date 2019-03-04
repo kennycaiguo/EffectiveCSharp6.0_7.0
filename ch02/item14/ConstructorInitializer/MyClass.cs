97cd5b7db4facf9fe3a75461582e5f8080345b11
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 15:47:13 +0900

    item14: ConsturctorInitializer

---
+++ ch02/item14/ConstructorInitializer/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorInitializer
+{
+    public struct ImportantData
+    {
+
+    }
+
+    public class MyClass
+    {
+        // コレクションデータ
+        private List<ImportantData> col1;
+
+        // インスタンス名
+        private string name;
+
+        public List<ImportantData> Collection { get { return col1; } }
+        public string Name { get { return name; } }
+
+        public MyClass() :
+            this(0, "")
+        {
+        }
+
+        public MyClass(int initialCount) :
+            this(initialCount, string.Empty)
+        {
+        }
+
+        public MyClass(int initialCount, string name)
+        {
+            col1 = (initialCount > 0) ?
+                new List<ImportantData>(initialCount) :
+                new List<ImportantData>();
+            this.name = name;
+        }
+    }
+}
