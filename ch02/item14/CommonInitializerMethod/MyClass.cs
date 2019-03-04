49c999fe811b08a60c310c607214e47f0230ce1d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-04 16:59:09 +0900

    item14: CommonInitializerMethod

---
+++ ch02/item14/CommonInitializerMethod/MyClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInitializerMethod
+{
+    public struct ImportantData
+    {
+
+    }
+
+    public class MyClass
+    {
+        // コレクションデータ
+        private List<ImportantData> col1;
+
+        // インスタンス名
+        private string name;
+
+        public List<ImportantData> Collection { get { return col1; } }
+        public string Name { get { return name; } }
+
+        public MyClass()
+        {
+            commonConstructor(0, "");
+        }
+
+        public MyClass(int initialCount)
+        {
+            commonConstructor(initialCount, "");
+        }
+
+        public MyClass(int initialCount, string name)
+        {
+            commonConstructor(initialCount, name);
+        }
+
+        private void commonConstructor(int count, string name)
+        {
+            col1 = (count > 0) ?
+                new List<ImportantData>(count) :
+                new List<ImportantData>();
+            this.name = name;
+        }
+    }
+}
---
+++ ch02/item14/CommonInitializerMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInitializerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var var1 = new MyClass();
+            var var2 = new MyClass(10);
+            var var3 = new MyClass(100, "Sophia");
+
+            Console.WriteLine($"var1.Name:{var1.Name}, var1.Collection.Capacity:{var1.Collection.Capacity}");
+            Console.WriteLine($"var2.Name:{var2.Name}, var2.Collection.Capacity:{var2.Collection.Capacity}");
+            Console.WriteLine($"var3.Name:{var3.Name}, var3.Collection.Capacity:{var3.Collection.Capacity}");
+        }
+    }
+}
