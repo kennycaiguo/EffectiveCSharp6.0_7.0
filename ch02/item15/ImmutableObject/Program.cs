34d6b085a1f862e46d5434c43d4bfd1ffdb3c124
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 05:58:46 +0900

    item15: ImmutableObject

---
+++ ch02/item15/ImmutableObject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmutableObject
+{
+    class Program
+    {
+        static User thisUser = new User { Name = "Sophia" };
+
+        static string UseStringConcatination()
+        {
+            string msg = "Hello, ";
+            msg += thisUser.Name;
+            msg += ". Today is ";
+            msg += System.DateTime.Now.ToString();
+
+            return msg;
+        }
+
+        static string UseStringInterpolation()
+        {
+            string msg = string.Format("Hello, {0}. Today is {1}",
+                thisUser.Name, DateTime.Now.ToString());
+
+            return msg;
+        }
+
+        static string UseStringBuilder()
+        {
+            StringBuilder msg = new StringBuilder("Hello, ");
+            msg.Append(thisUser.Name);
+            msg.Append(", Today is ");
+            msg.Append(DateTime.Now.ToString());
+            string finalMsg = msg.ToString();
+
+            return finalMsg;
+        }
+
+        static void Main(string[] args)
+        {
+            var msg = UseStringConcatination();
+            Console.WriteLine(msg);
+
+            msg = UseStringInterpolation();
+            Console.WriteLine(msg);
+
+            msg = UseStringBuilder();
+            Console.WriteLine(msg);
+        }
+    }
+}
