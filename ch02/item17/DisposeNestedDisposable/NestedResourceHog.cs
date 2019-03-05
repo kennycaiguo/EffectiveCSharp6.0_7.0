00a6cfd4d17be0d20df3a446b8929a73d23658bd
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 17:19:31 +0900

    item17: DisposeNestedDisposable

---
+++ ch02/item17/DisposeNestedDisposable/NestedResourceHog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeNestedDisposable
+{
+    class NestedResourceHog : IDisposable
+    {
+        // すでに破棄済みかどうかを示すフラグ
+        private bool alreadyDisposed = false;
+
+        // IDisposableの実装
+        // virtual Dispose(bool)を呼ぶ他にファイナライゼイションを抑制する
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // ファイナライザ
+        ~NestedResourceHog()
+        {
+            Console.WriteLine("finalizing NestedResourceHog");
+            Dispose(false);
+        }
+
+        // virtual Dispose(bool)メソッド
+        protected virtual void Dispose(bool isDisposing)
+        {
+            // 2回以上は破棄処理を行わないようにする
+            if (alreadyDisposed)
+            {
+                Console.WriteLine("NestedResourceHog: already disposed");
+                return;
+            }
+
+            if (isDisposing)
+            {
+                // この位置でマネージリソースを開放する
+                Console.WriteLine("releasing managed resource of NestedResourceHog");
+            }
+            // この位置で非マネージリソースを開放する
+            Console.WriteLine("releasing non-managed resource of NestedResourceHog");
+
+            // 破棄済みのフラグを設定
+            alreadyDisposed = true;
+        }
+    }
+}
---
+++ ch02/item17/DisposeNestedDisposable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeNestedDisposable
+{
+    class Program
+    {
+        static void DisposeParentDisposable()
+        {
+            Console.WriteLine("DisposeParentDisposable():");
+
+            using (var o = new MyResourceHog(new NestedResourceHog()))
+            {
+
+            }
+        }
+
+        static void DisposeParentThenNested()
+        {
+            Console.WriteLine("DisposeParentThenNested():");
+            var nested = new NestedResourceHog();
+            var parent = new MyResourceHog(nested);
+
+            parent.Dispose();
+            nested.Dispose();
+        }
+
+        static void DisposeNestedThenParent()
+        {
+            Console.WriteLine("DisposeNestedThenParent():");
+            var nested = new NestedResourceHog();
+            var parent = new MyResourceHog(nested);
+
+            nested.Dispose();
+            parent.Dispose();
+        }
+
+        static void Main(string[] args)
+        {
+            DisposeParentDisposable();
+            DisposeParentThenNested();
+            DisposeNestedThenParent();
+        }
+    }
+}
