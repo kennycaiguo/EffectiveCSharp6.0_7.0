f8689c7103fca1252f8561c1d43b188755ad6173
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 17:46:27 +0900

    item17: ReviveFinalizedObject

---
+++ ch02/item17/ReviveFinalizedObject/BadClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviveFinalizedObject
+{
+    public class BadClass
+    {
+        // グローバルオブジェクトの参照を保持
+        private static readonly List<BadClass> finalizedList = new List<BadClass>();
+        public static List<BadClass> FinalizedList
+        {
+            get { return finalizedList; }
+        }
+        private string msg;
+
+        public BadClass(string msg)
+        {
+            // 参照をキャッシュ
+            msg = (string)msg.Clone();
+        }
+
+        ~BadClass()
+        {
+            // オブジェクト自信をリストに追加
+            //　このオブジェクトは到達可能になるため、ガベージではなくなる
+            //　つまり復活します！
+            finalizedList.Add(this);
+        }
+    }
+}
---
+++ ch02/item17/ReviveFinalizedObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReviveFinalizedObject
+{
+    class Program
+    {
+        static void UseBadClassLocally()
+        {
+            Console.WriteLine("UseBadClassLocally():");
+            var badClass = new BadClass("local variable");
+        }
+
+        static void Main(string[] args)
+        {
+            UseBadClassLocally();
+            Console.WriteLine($"FinalizedList.Count: {BadClass.FinalizedList.Count}");
+
+            GC.Collect();
+            Thread.Sleep(10);
+            Console.WriteLine($"FinalizedList.Count: {BadClass.FinalizedList.Count}");
+        }
+    }
+}
