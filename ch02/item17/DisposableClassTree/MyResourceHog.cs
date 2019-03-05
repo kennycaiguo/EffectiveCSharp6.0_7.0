877c5ddac939256e050f94e857dccf3240ab4a9b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 11:54:35 +0900

    item17: DisposableClassTree

---
+++ ch02/item17/DisposableClassTree/MyResourceHog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableClassTree
+{
+    class MyResourceHog : IDisposable
+    {
+        // すでに破棄済みかどうかを示すフラグ
+        private bool alreadyDisposed = false;
+
+        // IDisposableの実装
+        // virtual Dispose(bool)を呼ぶ他にファイナライゼイションを抑制する
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // virtual Dispose(bool)メソッド
+        protected virtual void Dispose(bool isDisposing)
+        {
+            // 2回以上は破棄処理を行わないようにする
+            if (alreadyDisposed)
+                return;
+
+            if (isDisposing)
+            {
+                // この位置でマネージリソースを開放する
+                Console.WriteLine("release managed resource of MyResourceHog");
+            }
+            // この位置で非マネージリソースを開放する
+            Console.WriteLine("release non-managed resource of MyResourceHog");
+
+            // 破棄済みのフラグを設定
+            alreadyDisposed = true;
+        }
+
+        public void ExampleMethod()
+        {
+            if (alreadyDisposed)
+                throw new ObjectDisposedException(
+                    nameof(MyResourceHog),
+                    "破棄済みのオブジェクトでExampleMethodが呼ばれました");
+        
+            // その他のコードは省略
+        }
+    }
+}
---
+++ ch02/item17/DisposableClassTree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposableClassTree
+{
+    class Program
+    {
+        static void DisposeBaseClass()
+        {
+            Console.WriteLine("DisposeBaseClass():");
+            using (var o = new MyResourceHog())
+            {
+
+            }
+        }
+
+        static void UseClassAfterDisposed()
+        {
+            Console.WriteLine("UseClassAfterDisposed():");
+            var o = new MyResourceHog();
+            o.Dispose();
+            try
+            {
+                o.ExampleMethod();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DisposeBaseClass();
+            UseClassAfterDisposed();
+        }
+    }
+}
