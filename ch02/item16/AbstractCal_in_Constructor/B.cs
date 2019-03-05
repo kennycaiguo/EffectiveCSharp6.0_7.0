21d3b76d4da096cb2db60dd0d9d6333026cc0907
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 10:01:19 +0900

    item16: AbstractCall_in_Constructor

---
+++ ch02/item16/AbstractCal_in_Constructor/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractCall_in_Construcotor
+{
+    abstract class B
+    {
+        protected B()
+        {
+            VFunc();
+        }
+
+        protected abstract void VFunc();
+    }
+}
---
+++ ch02/item16/AbstractCal_in_Constructor/Derived.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractCall_in_Construcotor
+{
+    class Derived : B
+    {
+        private readonly string msg = "初期化子で設定";
+
+        public Derived(string msg)
+        {
+            this.msg = msg;
+        }
+
+        protected override void VFunc()
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void Main()
+        {
+            var d = new Derived("メイン内のコンストラクタ");
+        }
+    }
+}
