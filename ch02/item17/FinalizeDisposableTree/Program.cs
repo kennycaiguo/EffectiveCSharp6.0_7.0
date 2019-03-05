4b9da17a96e4d6a9312a85d9d3da7020b43e0de1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-05 17:01:56 +0900

    item17: FinalizeDisposableTree

---
+++ ch02/item17/FinalizeDisposableTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizeDisposableTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resource = new MyResourceHog();
+            var derivedResource = new DerivedResourceHog();
+
+            Console.WriteLine("finishing Main()");
+        }
+    }
+}
