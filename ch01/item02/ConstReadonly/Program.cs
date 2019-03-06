94bd2ee34532458a37f2a2805a640c545d2c3de8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-06 20:16:33 +0900

    item02: ConstReadonly: add comment

--- ch01/item02/ConstReadonly/Program.cs
+++ ch01/item02/ConstReadonly/Program.cs
@@ -8,9 +8,13 @@ namespace ConstReadonly
 {
     class Program
     {
+        // コンパイル時定数：
         public const int Millennium = 2000;
+
+        //　実行時定数：
         public static readonly int ThisYear = 2004;
 
+        //　コンパイル不可。readonlyを使用する必要がある
         //private const DateTime classCreation = new DateTime(2000, 1, 1, 0, 0, 0);
         private static readonly DateTime classCreation = new DateTime(2000, 1, 1, 0, 0, 0);
 
