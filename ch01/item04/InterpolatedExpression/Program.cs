cb710a1b06376061ac3ad501f6e565d00ef8b558
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-07 17:49:09 +0900

    item04: InterpolatedExpression

--- ch01/item04/InterpolatedExpression/Program.cs
+++ ch01/item04/InterpolatedExpression/Program.cs
@@ -6,29 +6,6 @@ using System.Threading.Tasks;
 
 namespace InterpolatedExpression
 {
-    public class Customer
-    {
-        public string Name { get; set; }
-    }
-
-    public class Records
-    {
-        public bool TryGetValue(int index, out string result)
-        {
-            switch (index % 3)
-            {
-                case 1:
-                    result = "rest 1";
-                    return true;
-                case 2:
-                    result = "rest 2";
-                    return true;
-                default:
-                    result = "unknown";
-                    return false;
-            }
-        }
-    }
     class Program
     {
         static void NullCoalescingOperator(Customer c)
@@ -50,8 +27,10 @@ namespace InterpolatedExpression
 
         static void Main(string[] args)
         {
-            Customer c = new Customer() { Name = "sasaki" };
+            Customer c = new Customer { Name = "sasaki" };
+            Customer c_null = new Customer { Name = null };
             NullCoalescingOperator(c);
+            NullCoalescingOperator(c_null);
             NullCoalescingOperator(null);
 
             Records records = new Records();
---
+++ ch01/item04/InterpolatedExpression/Record.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpolatedExpression
+{
+    public class Records
+    {
+        public bool TryGetValue(int index, out string result)
+        {
+            switch (index % 3)
+            {
+                case 1:
+                    result = "rest 1";
+                    return true;
+                case 2:
+                    result = "rest 2";
+                    return true;
+                default:
+                    result = "unknown";
+                    return false;
+            }
+        }
+    }
+}
