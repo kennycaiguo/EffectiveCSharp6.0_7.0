192b0aba49826324f5734e748aa1a29d403a4807
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-08 08:55:56 +0900

    item08: CheckNullHandler

---
+++ ch01/item08/CheckNullHandler/EventSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNullHandler
+{
+    public class EventSource
+    {
+        private EventHandler<int> Updated;
+        public void SetHandler(EventHandler<int> h)
+        {
+            Updated = h;
+        }
+        public void ClearHandler()
+        {
+            Updated = null;
+        }
+        public void RaiseUpdates()
+        {
+            counter++;
+            if (Updated != null)
+                Updated(this, counter);
+        }
+
+        private int counter;
+    }
+}
--- ch01/item08/CheckNullHandler/Program.cs
+++ ch01/item08/CheckNullHandler/Program.cs
@@ -6,32 +6,11 @@ using System.Threading.Tasks;
 
 namespace CheckNullHandler
 {
-    public class EventSource
-    {
-        private EventHandler<int> Updated;
-        public void SetHandler(EventHandler<int> h)
-        {
-            Updated = h;
-        }
-        public void ClearHandler()
-        {
-            Updated = null;
-        }
-        public void RaiseUpdates()
-        {
-            counter++;
-            if (Updated != null)
-                Updated(this, counter);
-        }
-
-        private int counter;
-    }
-
     class Program
     {
         static void Handler(Object sender, int param)
         {
-            Console.WriteLine("Handler called: param=" + param);
+            Console.WriteLine($"Handler called: param={param}");
         }
 
         static void Main(string[] args)
