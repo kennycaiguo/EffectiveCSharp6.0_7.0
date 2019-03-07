6aac7c9044d6d42f3b1db9676ec202294a7621ba
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-07 11:46:11 +0900

    item03: As: separate MyType class etc. into MyType.cs

--- ch01/item03/As/Program.cs
+++ ch01/item03/As/Program.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 
 namespace As
 {
-    class MyType
-    {
-
-    }
-    class MyType2
-    {
-
-    }
-    class Factory
-    {
-        public static object GetObject()
-        {
-            //return new MyType();
-            return new MyType2();
-        }
-    }
     class Program
     {
         static void Main(string[] args)
