5f8356aa97159c81f6d6fbd49e0c1e712363f248
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-08 16:15:35 +0900

    item09: MutableValueType: separate Person.cs

---
+++ ch01/item09/MutableValueType/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MutableValueType
+{
+    public struct Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- ch01/item09/MutableValueType/Program.cs
+++ ch01/item09/MutableValueType/Program.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 
 namespace MutableValueType
 {
-    public struct Person
-    {
-        public string Name { get; set; }
-
-        public override string ToString()
-        {
-            return Name;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
