a4b0dd86801a77cab6bd6ada25031be411568d38
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-10 10:19:40 +0900

    item18: Default

---
+++ ch03/item18/Default/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default
+{
+    class Program
+    {
+        static void TestInts()
+        {
+            Console.WriteLine("TestInts():");
+            var ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int result;
+
+            result = Utils.FirstOrDefault(ints, (i) => i % 7 == 0);
+            Console.WriteLine($"FirstOrDefault(ints, (i) => i % 7 == 0) : {result}");
+
+            result = Utils.FirstOrDefault(ints, (i) => i % 11 == 0);
+            Console.WriteLine($"FirstOrDefault(ints, (i) => i % 11 == 0) : {result}");
+        }
+
+        static void TestStrings()
+        {
+            Console.WriteLine("TestStrings():");
+            var strings = new List<string> { "abc", "def", "ghi", "jkl" };
+            string result;
+
+            result = Utils.FirstOrDefault(strings, (s) => s == "ghi");
+            Console.WriteLine($"FirstOrDefault((strings, (s) => s == \"ghi\")) : {result}");
+
+            result = Utils.FirstOrDefault(strings, (s) => s == "xyz");
+            Console.WriteLine($"FirstOrDefault((strings, (s) => s == \"xyz\")) : {result}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            TestInts();
+            TestStrings();
+        }
+    }
+}
---
+++ ch03/item18/Default/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default
+{
+    public static class Utils
+    {
+        public static T FirstOrDefault<T>(this IEnumerable<T> sequence,
+            Predicate<T> test)
+        {
+            foreach (T value in sequence)
+                if (test(value))
+                    return value;
+            return default(T);
+        }
+    }
+}
