8f333798f6e5f5418a94f5a49cb36eaed9888770
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-10 14:26:00 +0900

    item19: GerericAlgorithm

---
+++ ch03/item19/GenericAlgorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAlgorithm
+{
+    class Program
+    {
+        static void TestIntSequence()
+        {
+            Console.WriteLine("TestIntSequence():");
+            IEnumerable<int> intSequence = Enumerable.Range(1, 5);
+
+            Console.WriteLine("IntSequence:");
+            var reverseIntSequence = new ReverseEnumerable<int>(intSequence);
+            foreach (var item in reverseIntSequence)
+                Console.WriteLine(item);
+
+            Console.WriteLine("Sequence (cast IEnumerable<int> as IEnumerable):");
+            IEnumerable reverseSequence = new ReverseEnumerable<int>(intSequence) as IEnumerable;
+            foreach (var item in reverseSequence)
+                Console.WriteLine(item);
+        }
+
+        static void TestIntList()
+        {
+            Console.WriteLine("TestIntList():");
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("IntList:");
+            var reverseSequence = new ReverseEnumerable<int>(list);
+            foreach (var item in reverseSequence)
+                Console.WriteLine(item);
+
+            Console.WriteLine("IntSequence (cast List<int> as IEnumerable<int>):");
+            IEnumerable<int> reverseSequence2 = new ReverseEnumerable<int>(list) as IEnumerable<int>;
+            foreach (var item in reverseSequence2)
+                Console.WriteLine(item);
+        }
+
+        static void Main(string[] args)
+        {
+            TestIntSequence();
+            TestIntList();
+        }
+    }
+}
---
+++ ch03/item19/GenericAlgorithm/ReverseEnumerable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAlgorithm
+{
+    public sealed class ReverseEnumerable<T> : IEnumerable<T>
+    {
+        private sealed class ReverseEnuerator : IEnumerator<T>
+        {
+            int currentIndex;
+            IList<T> collection;
+
+            public ReverseEnuerator(IList<T> srcCollection)
+            {
+                collection = srcCollection;
+                currentIndex = collection.Count;
+            }
+
+            // IEnumerator<T>メンバ
+            public T Current => collection[currentIndex];
+
+            // IDisposal<T>メンバ
+            public void Dispose()
+            {
+                //　実装コードはないが、IEnumerator<T>がIDisposableを実装しているため必要
+                //　このクラスはsealedなのでprotected版のDispose()を実装する必要はない
+            }
+
+            // Enumeratorメンバ
+            object IEnumerator.Current => this.Current;
+
+            public bool MoveNext() => --currentIndex >= 0;
+            public void Reset() => currentIndex = collection.Count;
+        }
+
+        IEnumerable<T> sourceSequence;
+        IList<T> originalSequence;
+
+        public ReverseEnumerable(IEnumerable<T> sequence)
+        {
+            sourceSequence = sequence;
+        }
+
+        // IEnumerable<T>メンバ
+        public IEnumerator<T> GetEnumerator()
+        {
+            Console.WriteLine("GetEnumerator():");
+
+            //　元のシーケンスをコピーし、逆順にできるようにする
+            if (originalSequence == null)
+            {
+                Console.WriteLine(".. copy original sequence into new List<T>()");
+
+                originalSequence = new List<T>();
+                foreach (T item in sourceSequence)
+                {
+                    originalSequence.Add(item);
+                }
+            }
+            return new ReverseEnuerator(originalSequence);
+        }
+
+        // IEnumerableメンバ
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
