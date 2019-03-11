470ff3af7917c164caa4ccef5336f704d2f51d5c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-11 19:56:54 +0900

    item20: NaturalOrder

---
+++ ch03/item20/NaturalOrder/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalOrder
+{
+    public struct Customer : IComparable<Customer>, IComparable
+    {
+        private readonly string name;
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        // IComparable<Customer>メンバ
+        public int CompareTo(Customer other) => name.CompareTo(other.name);
+
+        // IComparableメンバ
+        //　2019.03.11：　注意：publicとすることはできない
+        int IComparable.CompareTo(object obj)
+        {
+            if (!(obj is Customer))
+                throw new ArgumentException("引数はCustomer型ではありません", "obj");
+            Customer otherCustomer = (Customer)obj;
+            return this.CompareTo(otherCustomer);
+        }
+
+        // 2019.03.11 add
+        public override string ToString()
+        {
+            return "{Name=" + this.name + "}";
+        }
+    }
+}
---
+++ ch03/item20/NaturalOrder/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalOrder
+{
+    class Program
+    {
+        static void CompareCustomer()
+        {
+            int diff = new Customer("Sophia").CompareTo(new Customer("Seiji"));
+
+            Console.WriteLine("new Customer(\"Sophia\").CompareTo(new Customer(\"Seiji\")): " + diff);
+      
+        }
+
+        static void CompareCustomerWithEmployee()
+        {
+            Customer c1 = new Customer("Sophia");
+            Employee e1 = new Employee { Name = "Sasaki" };
+
+            try
+            {
+                //if (c1.CompareTo(e1) > 0)
+                if (((IComparable)c1).CompareTo(e1) > 0)
+                    Console.WriteLine("1つ目のCustomerの方が大");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void SortCustomers()
+        {
+            List<Customer> list = new List<Customer>
+            { new Customer("Sophia"), new Customer("Seiji"), new Customer("Lisa") };
+
+            list.Sort();
+            foreach (var c in list)
+                Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            CompareCustomer();
+            CompareCustomerWithEmployee();
+            SortCustomers();
+        }
+    }
+}
