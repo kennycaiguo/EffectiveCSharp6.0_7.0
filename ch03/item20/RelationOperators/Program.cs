4f482c23febe1747a791af6cc37449f721012aa8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-11 20:42:00 +0900

    item20: RelationOperators

---
+++ ch03/item20/RelationOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sophia = new Customer("Sophia");
+            var sophia_dash = new Customer("Sophia");
+            var seiji = new Customer("Seiji");
+            bool result;
+
+            result = sophia < seiji;
+            Console.WriteLine($"shopia < seiji: {result}");
+            result = sophia <= seiji;
+            Console.WriteLine($"shopia <= seiji: {result}");
+            result = sophia >= seiji;
+            Console.WriteLine($"shopia >= seiji: {result}");
+            result = sophia > seiji;
+            Console.WriteLine($"shopia > seiji: {result}");
+
+            result = sophia < sophia_dash;
+            Console.WriteLine($"sophia < sophia_dash: {result}");
+            result = sophia <= sophia_dash;
+            Console.WriteLine($"sophia <= sophia_dash: {result}");
+            result = sophia >= sophia_dash;
+            Console.WriteLine($"sophia >= sophia_dash: {result}");
+            result = sophia > sophia_dash;
+            Console.WriteLine($"sophia > sophia_dash: {result}");
+
+        }
+    }
+}
