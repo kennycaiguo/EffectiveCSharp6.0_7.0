d979b409575ba37794031a8a8f538163a93446ee
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-12 19:50:50 +0900

    item21: WithoutOwnership

---
+++ ch03/item21/WithoutOwnership/EngineDriver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutOwnership
+{
+    public interface IEngine
+    {
+        void DoWork();
+    }
+
+    public sealed class EngineDriver<T> where T : IEngine
+    {
+        // 生成コストが高いため、nullに初期化
+        private T driver;
+        public EngineDriver(T driver)
+        {
+            this.driver = driver;
+        }
+        public void GetThingsDone()
+        {
+            driver.DoWork();
+        }
+    }
+}
---
+++ ch03/item21/WithoutOwnership/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutOwnership
+{
+    class Program
+    {
+        static void TestNonDisposableEngine()
+        {
+            Console.WriteLine("TestNonDisposableEngine():");
+
+            var engine = new NonDisposableEngine();
+            var driver = new EngineDriver<NonDisposableEngine>(engine);
+            driver.GetThingsDone();
+        }
+
+        static void TestDisposableEngine()
+        {
+            Console.WriteLine("TestDisposableEngine():");
+
+            using (var engine = new DisposableEngine())
+            {
+                var driver = new EngineDriver<DisposableEngine>(engine);
+                driver.GetThingsDone();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestNonDisposableEngine();
+            TestDisposableEngine();
+        }
+    }
+}
