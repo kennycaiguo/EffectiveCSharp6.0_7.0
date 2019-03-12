e9ca4bbbd3df4e6f4f85b949b4309eb6456c38fb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-13 06:36:39 +0900

    item22: ArrayVariance

---
+++ ch03/item22/ArrayVariance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVariance
+{
+    class Program
+    {
+        static void TestCoVariantArray()
+        {
+            Console.WriteLine("TestCoVariantArray():");
+            var array = new Planet[]
+            {
+                new Planet { Name="earch", Mass=1000},
+                new Planet{Name="jupitar", Mass=1000000 },
+                new Planet {Name="mars", Mass=50}
+            };
+            VariantArray.CoVariantArray(array);
+
+        }
+
+        static void TestUnsafeCoVariantArray()
+        {
+            Console.WriteLine("TestUnsafeCoVariantArray():");
+            var planets = new Planet[]
+            {
+                new Planet { Name="earch", Mass=1000},
+                new Planet{Name="jupitar", Mass=1000000 },
+                new Planet {Name="mars", Mass=50}
+            };
+            VariantArray.UnsafeCoVariantArray(planets);
+
+        }
+
+        static void Main(string[] args)
+        {
+            TestCoVariantArray();
+            TestUnsafeCoVariantArray();
+        }
+    }
+}
---
+++ ch03/item22/ArrayVariance/VariantArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayVariance
+{
+    public static class VariantArray
+    {
+        public static void CoVariantArray(CelestialBody[] baseItems)
+        {
+            foreach (var thing in baseItems)
+                Console.WriteLine("{0} の質量は {1} Kgです",
+                    thing.Name, thing.Mass);
+        }
+
+        public static void UnsafeCoVariantArray(CelestialBody[] baseItems)
+        {
+            baseItems[0] = new Asteroid { Name = "Hygiea", Mass = 8.85e19 };
+        }
+    }
+}
