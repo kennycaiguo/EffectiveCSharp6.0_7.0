69c0b3ae1aeebdd566ba8f1ad38ed4d3adb75302
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-13 16:54:04 +0900

    item21: ArrayVariance: assign Asteroid[5] to CelestialBody[] variable

--- ch03/item22/ArrayVariance/Program.cs
+++ ch03/item22/ArrayVariance/Program.cs
@@ -4,34 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ArrayVariance
 {
+    // 2019.03.13 add
+    using static VariantArray;
+
     class Program
     {
         static void TestCoVariantArray()
         {
             Console.WriteLine("TestCoVariantArray():");
-            var array = new Planet[]
-            {
-                new Planet { Name="earch", Mass=1000},
-                new Planet{Name="jupitar", Mass=1000000 },
-                new Planet {Name="mars", Mass=50}
-            };
-            VariantArray.CoVariantArray(array);
-
-        }
-
-        static void TestUnsafeCoVariantArray()
-        {
-            Console.WriteLine("TestUnsafeCoVariantArray():");
             var planets = new Planet[]
             {
                 new Planet { Name="earch", Mass=1000},
                 new Planet{Name="jupitar", Mass=1000000 },
                 new Planet {Name="mars", Mass=50}
             };
-            VariantArray.UnsafeCoVariantArray(planets);
+            CoVariantArray(planets);
 
+        }
+
+        static void TestUnsafeCoVariantArray()
+        {
+            Console.WriteLine("TestUnsafeCoVariantArray():");
+
+            var planets = new Planet[]
+            {
+                new Planet { Name="earch", Mass=1000},
+                new Planet { Name="jupitar", Mass=1000000 },
+                new Planet { Name="mars", Mass=50}
+            };
+            try
+            {
+                Console.WriteLine("try call UnsafeoVariantarray(planet):");
+                UnsafeCoVariantArray(planets);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("try assign Asteroid[] to CelestailBody[] variable:");
+            try
+            {
+                CelestialBody[] spaceJunk = new Asteroid[5];
+                spaceJunk[0] = new Planet();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         static void Main(string[] args)
