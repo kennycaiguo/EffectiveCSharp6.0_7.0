8dea5fe77a2ce093b891233cf7187e8fa533375f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-13 19:23:12 +0900

    item22: DelegateVariance

---
+++ ch03/item22/DelegateVariance/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateVariance
+{
+    class Program
+    {
+        static void TestICovariantDelegate()
+        {
+            Console.WriteLine("TestICovariantDelegata():");
+
+            var supplier = new IntSupplier(123);
+            int result;
+
+            result = supplier.GetAnItem();
+            Console.WriteLine($"supplier.GetAnItem(): {result}");
+
+            var func = supplier.GetAnItemLater();
+            result = func();
+            Console.WriteLine($"var func = supplier.GetAnItemLater(); func(): {result}");
+
+            Console.WriteLine("supplier.GiveAnItemLater((v) => Console.WriteLine(v)):");
+            supplier.GiveAnItemLater((v) => Console.WriteLine(v));
+        }
+
+        static void TestIContravariantDelegate()
+        {
+            Console.WriteLine("TestIContravariantDelegata():");
+
+            var consumer = new IntConsumer();
+            int result;
+            Action<int> action;
+
+            consumer.ActOnAnItem(123);
+            result = consumer.Value;
+            Console.WriteLine($"consumer.ActOnAnItem(123); consumer.Value: {result}");
+
+            consumer.GetAnItemLater(() => 456);
+            result = consumer.Value;
+            Console.WriteLine($"consumer.GetAnItemLater(() => 456); consumer.Value: {result}");
+
+            action = consumer.ActOnAnItemLater();
+            action(789);
+            result = consumer.Value;
+            Console.WriteLine($"action = consumer.ActOnAnItemLater(); action(789); consumer.Value: {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            TestICovariantDelegate();
+            TestIContravariantDelegate();
+        }
+    }
+}
---
+++ ch03/item22/DelegateVariance/VariantDelegate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateVariance
+{
+    public interface ICovariantDelegate<out T>
+    {
+        T GetAnItem();
+        Func<T> GetAnItemLater();
+        void GiveAnItemLater(Action<T> whatToDo);
+    }
+
+    public interface IContravariantDelegate<in T>
+    {
+        void ActOnAnItem(T item);
+        void GetAnItemLater(Func<T> item);
+        Action<T> ActOnAnItemLater();
+    }
+
+    public class IntSupplier : ICovariantDelegate<int>
+    {
+        private int value;
+        public IntSupplier(int value)
+        {
+            this.value = value;
+        }
+
+        public int GetAnItem()
+        {
+            return this.value;
+        }
+
+        public Func<int> GetAnItemLater()
+        {
+            return () => this.value;
+        }
+
+        public void GiveAnItemLater(Action<int> whatToDo)
+        {
+            whatToDo(value);
+        }
+    }
+
+    public class IntConsumer : IContravariantDelegate<int>
+    {
+        private int value;
+        public int Value
+        {
+            get { return value; }
+        }
+
+        public void ActOnAnItem(int item)
+        {
+            this.value = item;
+        }
+
+        public Action<int> ActOnAnItemLater()
+        {
+            return (item) => this.value = item; 
+        }
+
+        public void GetAnItemLater(Func<int> item)
+        {
+            this.value = item();
+        }
+    }
+
+}
