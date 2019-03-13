2b6c6fd3d63690b9a19fc5c9328c860626634a8e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-13 18:25:23 +0900

    item22: ContraVariance

---
+++ ch03/item22/ContraVariance/CelestialBody.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContraVariance
+{
+    abstract public class CelestialBody : IComparable<CelestialBody>
+    {
+        public double Mass { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(CelestialBody other)
+        {
+            if (other == null)
+                return 1;
+            return Mass.CompareTo(other.Mass);
+        }
+    }
+
+    public class Planet : CelestialBody
+    {
+        // 省略
+    }
+
+    public class Moon : CelestialBody
+    {
+        // 省略
+    }
+    public class Asteroid : CelestialBody
+    {
+        // 省略
+    }
+}
---
+++ ch03/item22/ContraVariance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContraVariance
+{
+    class Program
+    {
+        static void CompareCelestialBody()
+        {
+            Console.WriteLine("CompareCelestialBody():");
+            var earch = new Planet { Name = "Earch", Mass = 5.5e22 };
+            var moon = new Moon { Name = "Moon", Mass = 2.2e21 };
+            var hygioa = new Asteroid { Name = "Hygia", Mass = 8.85e19 };
+            int result;
+
+            result = earch.CompareTo(moon);
+            Console.WriteLine($"earch.CompareTo(moon): {result}");
+
+            result = moon.CompareTo(hygioa);
+            Console.WriteLine($"moon.CompareTo(hygioa): {result}");
+
+            result = hygioa.CompareTo(earch);
+            Console.WriteLine($"hygioa.CompareTo(earch): {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            CompareCelestialBody();
+        }
+    }
+}
