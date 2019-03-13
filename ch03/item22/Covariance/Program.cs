575be28a0a03fef6b23eb38e712dfce3eac73197
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-13 17:19:23 +0900

    item22: Covariance

---
+++ ch03/item22/Covariance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariance
+{
+    using static VariantGeneric;
+    class Program
+    {
+        static void TestCovariantGeneric()
+        {
+            Console.WriteLine("TestCovariantGeneric():");
+            IEnumerable<Planet> planets = new List<Planet>
+            {
+                new Planet { Name="earch", Mass=1000},
+                new Planet{Name="jupitar", Mass=1000000 },
+                new Planet {Name="mars", Mass=50}
+            };
+            CovariantGeneric(planets);
+        }
+
+        static void TestInvariantGeneric()
+        {
+            Console.WriteLine("TestInvariantGeneric():");
+            IList<Planet> planets = new List<Planet>
+            {
+                new Planet { Name="earch", Mass=1000},
+                new Planet{Name="jupitar", Mass=1000000 },
+                new Planet {Name="mars", Mass=50}
+            };
+            // compile error
+            //InvariantGeneric(planets);
+            // 'System.Collections.Generic.IList<Covariance.Planet>' から 
+            // 'System.Collections.Generic.IList<Covariance.CelestialBody>' 
+            // へ変換することはできません。
+        }
+
+        static void Main(string[] args)
+        {
+            TestCovariantGeneric();
+            TestInvariantGeneric();
+        }
+    }
+}
---
+++ ch03/item22/Covariance/VariantGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariance
+{
+    public class VariantGeneric
+    {
+        public static void CovariantGeneric(IEnumerable<CelestialBody> baseItems)
+        {
+            foreach (var thing in baseItems)
+            {
+                Console.WriteLine("{0} の質量は {1} Kgです",
+                    thing.Name, thing.Mass);
+            }
+        }
+
+        public static void InvariantGeneric(IList<CelestialBody> baseItems)
+        {
+            baseItems[0] = new Asteroid { Name = "Hygiea", Mass = 8.85e19 };
+        }
+    }
+}
