db5d059314ba3d07e31e3e566fbf490d7eff4321
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-14 19:42:29 +0900

    item23: InterfaceAsFuncArg

---
+++ ch03/item23/InterfaceAsFuncArg/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAsFuncArg
+{
+    public static class Example
+    {
+        public static T Add<T>(T left, T right, IAdd<T> Callback) =>
+            Callback.Add(left, right);
+    }
+}
---
+++ ch03/item23/InterfaceAsFuncArg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAsFuncArg
+{
+    class AddInt : IAdd<int>
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 7;
+
+            int sum = Example.Add(a, b, new AddInt());
+            Console.WriteLine($"sum: {sum}");
+        }
+    }
+}
