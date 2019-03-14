22d55de8e2aa40bfaa1ba81238db7366c299be49
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-14 19:29:17 +0900

    item23: DelegateAsFuncArg

---
+++ ch03/item23/DelegateAsFuncArg/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAsFuncArg
+{
+    public static class Example
+    {
+        public static T Add<T>(T left, T right, Func<T, T, T> AddFunc) =>
+            AddFunc(left, right);
+    }
+}
---
+++ ch03/item23/DelegateAsFuncArg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAsFuncArg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 7;
+            int sum = Example.Add(a, b, (x, y) => x + y);
+            Console.WriteLine($"sum: {sum}");
+        }
+    }
+}
