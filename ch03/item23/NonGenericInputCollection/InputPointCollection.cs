2e38a7061fea7ca40d703a0c68526a37d16ba7d7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-14 21:45:16 +0900

    item23: NonGenericInputCollection

---
+++ ch03/item23/NonGenericInputCollection/InputPointCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericInputCollection
+{
+    class InputPointCollection
+    {
+        private List<Point> thingsRead = new List<Point>();
+        public void ReadFromStream(TextReader reader) =>
+            thingsRead.Add(new Point(reader));
+
+        public IEnumerable<Point> Values => thingsRead;
+    }
+}
