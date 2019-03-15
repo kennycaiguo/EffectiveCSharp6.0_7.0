67cdddaa4a3284f1a1a03de8a610368c17c35cb0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-15 22:07:31 +0900

    item24: SpecializeBase

---
+++ ch03/item24/SpecializeBase/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace SpecializeBase
+{
+    class Program
+    {
+        static void WriteMessage(MyBase b)
+        {
+            WriteLine("WriteMessage(MyBase) 内");
+        }
+
+        static void WriteMessage<T>(T obj)
+        {
+            Write("WriteMessage<T>(T) 内: ");
+            WriteLine(obj.ToString());
+        }
+
+        static void WriteMessage(IMessageWriter obj)
+        {
+            Write("WriteMessage(IMessageWrite) 内: ");
+            obj.WriteMessage(); // virtual call
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerived d = new MyDerived();
+            WriteLine("Program.WriteMessage を呼び出し中");
+            WriteMessage(d);
+            WriteLine();
+
+            WriteLine("IMessageWriter インターフェース経由で呼び出し中");
+            WriteMessage((IMessageWriter)d);
+            WriteLine();
+
+            WriteLine("親クラスにキャスト");
+            WriteMessage((MyBase)d);
+            WriteLine();
+
+            WriteLine("別の型をテスト");
+            AnotherType anObject = new AnotherType();
+            WriteMessage(anObject);
+            WriteLine();
+
+            WriteLine("IMessageWriter にキャスト");
+            WriteMessage((IMessageWriter)anObject);
+        }
+    }
+}
