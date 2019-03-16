e56506c97ccf06d4e2ab366b7acccce4feb10fbb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-16 09:15:54 +0900

    item25: NonGenericStaticUtility

---
+++ ch03/item25/NonGenericStaticUtility/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericStaticUtility
+{
+    class Program
+    {
+        static void TestDouble()
+        {
+            double d1 = 4;
+            double d2 = 5;
+            double max = Utils.Max(d1, d2);
+            double min = Utils.Min(d1, d2);
+
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+        }
+
+        static void TestString()
+        {
+            string foo = "foo";
+            string bar = "bar";
+            string sMax = Utils.Max(foo, bar);
+            string sMin = Utils.Min(foo, bar);
+
+            Console.WriteLine($"Max: {sMax}");
+            Console.WriteLine($"Min: {sMin}");
+        }
+
+        static void TestNullableDouble()
+        {
+            double? d1 = 12;
+            double? d2 = null;
+            // 2019.03.16 change
+            //double? max = Utils.Max(d1, d2).Value;
+            double? max = Utils.Max(d1, d2);
+            double? min = Utils.Min(d1, d2);
+
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+        }
+
+        static void Main(string[] args)
+        {
+            TestDouble();
+            TestString();
+            TestNullableDouble();
+        }
+    }
+}
---
+++ ch03/item25/NonGenericStaticUtility/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericStaticUtility
+{
+    public static class Utils
+    {
+        public static T Max<T>(T left, T right) =>
+            Comparer<T>.Default.Compare(left, right) < 0 ? right : left;
+
+        public static double Max(double left, double right) =>
+            Math.Max(left, right);
+        // その他の数値型に対応するバージョンは省略
+
+        public static T Min<T>(T left, T right) =>
+            Comparer<T>.Default.Compare(left, right) < 0 ? left : right;
+
+        public static double Min(double left, double right) =>
+            Math.Min(left, right);
+        // その他の数値型に対応するバージョンは省略
+    }
+}
