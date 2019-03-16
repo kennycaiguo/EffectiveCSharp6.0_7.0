9a8df02086a31c36009edab0287cc9cc77cd5e60
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-16 09:56:35 +0900

    item25: NonGenericClassGenericMethod

---
+++ ch03/item25/NonGenericClassGeneicMethod/CommaSeparatedListBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericClassGeneicMethod
+{
+    public class CommaSeparatedListBuilder
+    {
+        private StringBuilder storage = new StringBuilder();
+
+        public void Add<T>(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                if (storage.Length > 0)
+                    storage.Append(", ");
+                storage.Append("\"");
+                storage.Append(item.ToString());
+                storage.Append("\"");
+            }
+        }
+
+        public override string ToString() => storage.ToString();
+    }
+}
---
+++ ch03/item25/NonGenericClassGeneicMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericClassGeneicMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dList = new List<double> { 1.2, 3.4, 5.6 };
+            var sList = new List<string> { "abc", "xyz", "$%&" };
+            var builder = new CommaSeparatedListBuilder();
+            builder.Add(dList);
+            builder.Add(sList);
+            var result = builder.ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
