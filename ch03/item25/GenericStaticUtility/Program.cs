09a0838cd3cc14d64f610fb330d9c70dd30c3bf4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-16 09:01:10 +0900

    item25: GernericStaticUtility

---
+++ ch03/item25/GenericStaticUtility/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStaticUtility
+{
+    class Program
+    {
+        static void TestDouble()
+        {
+            double d1 = 4;
+            double d2 = 5;
+            double max = Utils<double>.Max(d1, d2);
+            double min = Utils<double>.Min(d1, d2);
+
+            Console.WriteLine($"Max: {max}");
+            Console.WriteLine($"Min: {min}");
+        }
+
+        static void TestString()
+        {
+            string foo = "foo";
+            string bar = "bar";
+            string sMax = Utils<string>.Max(foo, bar);
+            string sMin = Utils<string>.Min(foo, bar);
+
+            Console.WriteLine($"Max: {sMax}");
+            Console.WriteLine($"Min: {sMin}");
+        }
+
+        static void Main(string[] args)
+        {
+            TestDouble();
+            TestString();
+        }
+    }
+}
---
+++ ch03/item25/GenericStaticUtility/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStaticUtility
+{
+    public static class Utils<T>
+    {
+        public static T Max(T left, T right) =>
+            Comparer<T>.Default.Compare(left, right) < 0 ? right : left;
+
+        public static T Min(T left, T right) =>
+            Comparer<T>.Default.Compare(left, right) < 0 ? left : right;
+    }
+}
