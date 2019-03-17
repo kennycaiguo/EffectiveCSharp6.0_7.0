4515ad6506501abbcb7f9a41ef3e51d310ffab50
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-17 16:56:51 +0900

    item26: GenericInterfaceVersion: add TestEqualityOperations()

--- ch03/item26/GenericInterfaceVersion/Program.cs
+++ ch03/item26/GenericInterfaceVersion/Program.cs
@@ -161,6 +161,59 @@ namespace GenericInterfaceVersion
             Console.WriteLine($"name_a_a_a.Equals(name_a_a_a_dash): {result}");
         }
 
+        static void TestEqualityOperators()
+        {
+            Console.WriteLine("\nTestEqualityOperators():\n");
+
+            bool result;
+            Name name_null = null;
+            Name name_n_n_n = new Name();
+            Name name_a_n_n = new Name { Last = "a" };
+            Name name_b_n_n = new Name { Last = "b" };
+            Name name_n_a_n = new Name { First = "a" };
+            Name name_n_b_n = new Name { First = "b" };
+            Name name_n_n_a = new Name { Middle = "a" };
+            Name name_n_n_b = new Name { Middle = "b" };
+            Name name_a_a_a = new Name { Last = "a", First = "a", Middle = "a" };
+            Name name_a_a_a_dash = new Name { Last = "a", First = "a", Middle = "a" };
+
+            result = name_n_n_n == name_n_n_n;
+            Console.WriteLine($"name_n_n_n == name_n_n_n: {result}");
+
+            result = name_n_n_n != name_n_n_n;
+            Console.WriteLine($"name_n_n_n != name_n_n_n: {result}");
+
+            result = name_n_n_n == name_null;
+            Console.WriteLine($"name_n_n_n == name_null: {result}");
+
+            result = name_n_n_n != name_null;
+            Console.WriteLine($"name_n_n_n != name_null: {result}");
+
+            result = name_a_n_n == name_b_n_n;
+            Console.WriteLine($"name_a_n_n == name_b_n_n: {result}");
+
+            result = name_a_n_n != name_b_n_n;
+            Console.WriteLine($"name_a_n_n != name_b_n_n: {result}");
+
+            result = name_n_a_n == name_n_b_n;
+            Console.WriteLine($"name_n_a_n == name_n_b_n: {result}");
+
+            result = name_n_a_n != name_n_b_n;
+            Console.WriteLine($"name_n_a_n != name_n_b_n: {result}");
+
+            result = name_n_n_a == name_n_n_b;
+            Console.WriteLine($"name_n_n_a == name_n_n_b: {result}");
+
+            result = name_n_n_a != name_n_n_b;
+            Console.WriteLine($"name_n_n_a != name_n_n_b: {result}");
+
+            result = name_a_a_a == name_a_a_a_dash;
+            Console.WriteLine($"name_a_a_a == name_a_a_a_dash: {result}");
+
+            result = name_a_a_a != name_a_a_a_dash;
+            Console.WriteLine($"name_a_a_a != name_a_a_a_dash: {result}");
+        }
+
         static void Main(string[] args)
         {
             TestCompareTo();
@@ -169,6 +222,8 @@ namespace GenericInterfaceVersion
             TestCompareToDerived();
 
             TestEquals();
+            TestEqualityOperators();
+
         }
     }
 }
