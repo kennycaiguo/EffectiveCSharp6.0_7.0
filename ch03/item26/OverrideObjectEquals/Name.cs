1b419189447b402acef9764bdc7ad4a7b9982256
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-17 20:00:06 +0900

    item26: OverrideObjectEquals

---
+++ ch03/item26/OverrideObjectEquals/Name.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideObjectEquals
+{
+    public class Name :
+        IComparable<Name>,
+        IEquatable<Name>
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Middle { get; set; }
+
+        // IComparable<Name>メンバ
+        public int CompareTo(Name other)
+        {
+            if (Object.ReferenceEquals(this, other))
+                return 0;
+            if (Object.ReferenceEquals(other, null))
+                return 1; // 非nullはnullより大
+            int rVal = Comparer<string>.Default.Compare(Last, other.Last);
+            if (rVal != 0)
+                return rVal;
+            rVal = Comparer<string>.Default.Compare(First, other.First);
+            if (rVal != 0)
+                return rVal;
+            return Comparer<string>.Default.Compare(Middle, other.Middle);
+        }
+
+        // IEquatable<Name>メンバ
+        public bool Equals(Name other)
+        {
+            if (Object.ReferenceEquals(this, other))
+                return true;
+            if (Object.ReferenceEquals(other, null))
+                return false;
+            // 意味的にはEqualityComparer<string>.Defaultと同じ
+            return Last == other.Last &&
+                First == other.First &&
+                Middle == other.Middle;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // 2019.03.17 add: check obj == null
+            if (obj == null)
+                return false;
+
+            if (obj.GetType() == typeof(Name))
+                return this.Equals(obj as Name);
+            else return false;
+        }
+
+        public override string ToString()
+        {
+            return $"(Last={Last}, First={First}, Middle={Middle})";
+        }
+
+        // その他の詳細は省略
+    }
+}
---
+++ ch03/item26/OverrideObjectEquals/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideObjectEquals
+{
+    using static Utils;
+
+    class Program
+    {
+        static void TestCheckEquality()
+        {
+            Console.WriteLine("\nTestCheckEquality():\n");
+
+            bool result;
+            Name name_null = null;
+            Name name_n_n_n = new Name();
+            Name name_a_n_n = new Name { Last = "a" };
+            Name name_b_n_n = new Name { Last = "b" };
+            Name name_n_a_n = new Name { First = "a" };
+            Name name_n_b_n = new Name { First = "b" };
+            Name name_n_n_a = new Name { Middle = "a" };
+            Name name_n_n_b = new Name { Middle = "b" };
+            Name name_a_a_a = new Name { Last = "a", First = "a", Middle = "a" };
+            Name name_a_a_a_dash = new Name { Last = "a", First = "a", Middle = "a" };
+
+            result = CheckEquality(name_n_n_n, name_n_n_n);
+            Console.WriteLine($"CheckEquality(name_n_n_n, name_n_n_n): {result}");
+
+            result = CheckEquality(name_n_n_n, name_null);
+            Console.WriteLine($"CheckEquality(name_n_n_n, name_null): {result}");
+
+            result = CheckEquality(name_a_n_n, name_b_n_n);
+            Console.WriteLine($"CheckEquality(name_a_n_n, name_b_n_n): {result}");
+
+            result = CheckEquality(name_n_a_n, name_n_b_n);
+            Console.WriteLine($"CheckEquality(name_n_a_n, name_n_b_n): {result}");
+
+            result = CheckEquality(name_n_n_a, name_n_n_b);
+            Console.WriteLine($"CheckEquality(name_n_n_a, name_n_n_b): {result}");
+
+            result = CheckEquality(name_a_a_a, name_a_a_a_dash);
+            Console.WriteLine($"CheckEquality(name_a_a_a, name_a_a_a_dash): {result}");
+        }
+
+        static void TestEqualityOperators()
+        {
+            Console.WriteLine("\nTestEqualityOperators():\n");
+
+            bool result;
+            Name name_null = null;
+            Name name_n_n_n = new Name();
+            Name name_a_n_n = new Name { Last = "a" };
+            Name name_b_n_n = new Name { Last = "b" };
+            Name name_n_a_n = new Name { First = "a" };
+            Name name_n_b_n = new Name { First = "b" };
+            Name name_n_n_a = new Name { Middle = "a" };
+            Name name_n_n_b = new Name { Middle = "b" };
+            Name name_a_a_a = new Name { Last = "a", First = "a", Middle = "a" };
+            Name name_a_a_a_dash = new Name { Last = "a", First = "a", Middle = "a" };
+
+            result = name_n_n_n == name_n_n_n;
+            Console.WriteLine($"name_n_n_n == name_n_n_n: {result}");
+
+            result = name_n_n_n != name_n_n_n;
+            Console.WriteLine($"name_n_n_n != name_n_n_n: {result}");
+
+            result = name_n_n_n == name_null;
+            Console.WriteLine($"name_n_n_n == name_null: {result}");
+
+            result = name_n_n_n != name_null;
+            Console.WriteLine($"name_n_n_n != name_null: {result}");
+
+            result = name_a_n_n == name_b_n_n;
+            Console.WriteLine($"name_a_n_n == name_b_n_n: {result}");
+
+            result = name_a_n_n != name_b_n_n;
+            Console.WriteLine($"name_a_n_n != name_b_n_n: {result}");
+
+            result = name_n_a_n == name_n_b_n;
+            Console.WriteLine($"name_n_a_n == name_n_b_n: {result}");
+
+            result = name_n_a_n != name_n_b_n;
+            Console.WriteLine($"name_n_a_n != name_n_b_n: {result}");
+
+            result = name_n_n_a == name_n_n_b;
+            Console.WriteLine($"name_n_n_a == name_n_n_b: {result}");
+
+            result = name_n_n_a != name_n_n_b;
+            Console.WriteLine($"name_n_n_a != name_n_n_b: {result}");
+
+            result = name_a_a_a == name_a_a_a_dash;
+            Console.WriteLine($"name_a_a_a == name_a_a_a_dash: {result}");
+
+            result = name_a_a_a != name_a_a_a_dash;
+            Console.WriteLine($"name_a_a_a != name_a_a_a_dash: {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            TestCheckEquality();
+            TestEqualityOperators();
+        }
+    }
+}
