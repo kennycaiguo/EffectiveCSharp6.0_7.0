d0b896c11f23ed2a32b0c6d925f45b05fbd735af
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-17 18:53:28 +0900

    item26: OverloadCheckEquality

---
+++ ch03/item26/OverloadCheckEquality/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadCheckEquality
+{
+    public static class Utils
+    {
+        public static bool CheckEquality(object left, object right)
+        {
+            if (left == null)
+                return right == null;
+            return left.Equals(right);
+        }
+
+        public static bool CheckEquality<T>(T left, T right)
+            where T : IEquatable<T>
+        {
+            if (left == null)
+                return right == null;
+            return left.Equals(right);
+        }
+    }
+}
