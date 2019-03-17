67cad611c6fe5a18011923d74b94ae4b7f068f0a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-17 18:43:08 +0900

    item26: CheckEqualityUtility

---
+++ ch03/item26/CheckEqualityUtility/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEqualityUtility
+{
+    public static class Utils
+    {
+        public static bool CheckEquality(object left, object right)
+        {
+            if (left == null)
+                return right == null;
+            return left.Equals(right);
+        }
+    }
+}
