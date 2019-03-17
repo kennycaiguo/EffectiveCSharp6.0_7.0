65335deedd5fda3c417713fe88725002e1a310ae
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-17 18:20:22 +0900

    item26: GenericInterfaceVersion: add TestDictionaryKey()

--- ch03/item26/GenericInterfaceVersion/Program.cs
+++ ch03/item26/GenericInterfaceVersion/Program.cs
@@ -214,6 +214,25 @@ namespace GenericInterfaceVersion
             Console.WriteLine($"name_a_a_a != name_a_a_a_dash: {result}");
         }
 
+        static void TestDictionaryKey()
+        {
+            Console.WriteLine("\nTestDictionaryKey():\n");
+
+            bool result;
+            string value;
+            var myTable = new Dictionary<Name, string>();
+            Name name_a_a_a = new Name { Last = "a", First = "a", Middle = "a" };
+            Name name_a_a_a_dash = new Name { Last = "a", First = "a", Middle = "a" };
+
+            myTable.Add(name_a_a_a, "name_a_a_a");
+
+            result = myTable.TryGetValue(name_a_a_a, out value);
+            Console.WriteLine($"result = myTable.TryGetValue(name_a_a_a, out value); result: {result}, value: {value}");
+
+            result = myTable.TryGetValue(name_a_a_a_dash, out value);
+            Console.WriteLine($"result = myTable.TryGetValue(name_a_a_a_dash, out value); result: {result}, value: {value}");
+        }
+
         static void Main(string[] args)
         {
             TestCompareTo();
@@ -223,7 +242,7 @@ namespace GenericInterfaceVersion
 
             TestEquals();
             TestEqualityOperators();
-
+            TestDictionaryKey();
         }
     }
 }
