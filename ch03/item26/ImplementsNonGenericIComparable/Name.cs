dc0072a5138e2c6bc54b90bb7e8153c1b032961b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-18 20:26:29 +0900

    item26: ImplementsNonGenericIComparable

---
+++ ch03/item26/ImplementsNonGenericIComparable/Name.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementsNonGenericIComparable
+{
+    public class Name :
+        IComparable<Name>,
+        IEquatable<Name>,
+        IComparable
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Middle { get; set; }
+
+        // IComparable<Name>メンバ
+        public int CompareTo(Name other)
+        {
+            if (Object.ReferenceEquals(this, other))
+                return 0;
+            if (Object.ReferenceEquals(other, null))
+                return 1; // 非nullはnullより大
+            int rVal = Comparer<string>.Default.Compare(Last, other.Last);
+            if (rVal != 0)
+                return rVal;
+            rVal = Comparer<string>.Default.Compare(First, other.First);
+            if (rVal != 0)
+                return rVal;
+            return Comparer<string>.Default.Compare(Middle, other.Middle);
+        }
+
+        // IEquatable<Name>メンバ
+        public bool Equals(Name other)
+        {
+            if (Object.ReferenceEquals(this, other))
+                return true;
+            if (Object.ReferenceEquals(other, null))
+                return false;
+            // 意味的にはEqualityComparer<string>.Defaultと同じ
+            return Last == other.Last &&
+                First == other.First &&
+                Middle == other.Middle;
+        }
+
+        public override bool Equals(object obj)
+        {
+            // 2019.03.17 add: check obj == null
+            if (obj == null)
+                return false;
+
+            if (obj.GetType() == typeof(Name))
+                return this.Equals(obj as Name);
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            if (Last != null)
+                hashCode ^= Last.GetHashCode();
+            if (First != null)
+                hashCode ^= First.GetHashCode();
+            if (Middle != null)
+                hashCode ^= Middle.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Name left, Name right)
+        {
+            /* 2019.03.17 change: stack overflow
+            if (left == null)
+                return right == null;
+            */
+            if (Object.ReferenceEquals(left, null))
+                return Object.ReferenceEquals(right, null);
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Name left, Name right)
+        {
+            /* 2019.03.17 change
+            if (left == null)
+                return right != null;
+            */
+            if (Object.ReferenceEquals(left, null))
+                return !Object.ReferenceEquals(right, null);
+            return !left.Equals(right);
+        }
+
+        public int CompareTo(object obj)
+        {
+            // IComparableメンバ
+            // 2019.03.18: change: check obj not null and is Name
+            //if (obj.GetType() != typeof(Name))
+            if (!(obj is Name))
+                throw new ArgumentException(
+                    "引数はNameオブジェクトではありません");
+            return this.CompareTo(obj as Name);
+        }
+
+        public override string ToString()
+        {
+            return $"(Last={Last}, First={First}, Middle={Middle})";
+        }
+
+        // その他の詳細は省略
+    }
+}
---
+++ ch03/item26/ImplementsNonGenericIComparable/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementsNonGenericIComparable
+{
+    class Program
+    {
+        static void TestNonGenericSort()
+        {
+            Console.WriteLine("\nTestNonGenericSort():\n");
+
+            var list = new ArrayList
+            {
+                new Name { Last = "a" }, new Name { Last = "b" },
+                new Name { First = "a" }, new Name { First = "b" },
+                new Name { Middle = "a" }, new Name { Middle = "b" }
+            };
+
+            try
+            {
+                list.Sort();
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void TestNonGenericCompareTo()
+        {
+            Console.WriteLine("\nTestNonGenericCompareto():\n");
+
+            int result;
+            IComparable name_null = null;
+            IComparable name_n_n_n = new Name();
+            IComparable name_a_n_n = new Name { Last = "a" };
+            IComparable name_a_n_n_dash = new Name { Last = "a" };
+            IComparable name_b_n_n = new Name { Last = "b" };
+            IComparable derived_a_n_n__nick = new DerivedName { Last = "a", NickName = "nick" };
+            IComparable derived_b_n_n__nick = new DerivedName { Last = "b", NickName = "nick" };
+            IComparable string_a = "a";
+
+            try
+            {
+                result = name_a_n_n.CompareTo(name_null);
+                Console.WriteLine($"name_a_n_n.CompareTo(name_null): {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            result = name_a_n_n.CompareTo(name_a_n_n);
+            Console.WriteLine($"name_a_n_n.CompareTo(name_a_n_n): {result}");
+
+            result = name_a_n_n.CompareTo(name_b_n_n);
+            Console.WriteLine($"name_a_n_n.CompareTo(name_b_n_n): {result}");
+
+            result = name_b_n_n.CompareTo(name_a_n_n);
+            Console.WriteLine($"name_b_n_n.CompareTo(name_a_n_n): {result}");
+
+            result = name_a_n_n.CompareTo(name_a_n_n_dash);
+            Console.WriteLine($"name_a_n_n.CompareTo(name_a_n_n_dash): {result}");
+
+            try
+            {
+                result = name_a_n_n.CompareTo(derived_a_n_n__nick);
+                Console.WriteLine($"name_a_n_n.CompareTo(derived_a_n_n__nick): {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            result = name_a_n_n.CompareTo(derived_b_n_n__nick);
+            Console.WriteLine($"name_a_n_n.CompareTo(derived_b_n_n__nick): {result}");
+
+            result = name_b_n_n.CompareTo(derived_a_n_n__nick);
+            Console.WriteLine($"name_b_n_n.CompareTo(derived_a_n_n__nick): {result}");
+
+            try
+            {
+                result = name_a_n_n.CompareTo(string_a);
+                Console.WriteLine($"name_a_n_n.CompareTo(string_a): {result}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestNonGenericSort();
+            TestNonGenericCompareTo();
+        }
+    }
+}
