a1c982fe7b71ab1a34ad6729c3baaf2a5c773d3d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-21 10:42:22 +0900

    item29: ExtensionsForIEnumerableCustomer

---
+++ ch03/item28/ExtensionForIEnumerableCustomer/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionForIEnumerableCustomer
+{
+    public static class Extensions
+    {
+        public static void SendEmailCoupons(this IEnumerable<Customer> customers, 
+            Coupon spectialOffer)
+        {
+            foreach (var c in customers)
+            {
+                Utils.SendEmail(c.Name, c.Email, spectialOffer.Message);
+            }
+        }
+
+        public static IEnumerable<Customer> LostProspects(this IEnumerable<Customer> targetList)
+        {
+            IEnumerable<Customer> answer =
+                from c in targetList
+                where DateTime.Now - c.LastOrderDate > TimeSpan.FromDays(30)
+                select c;
+            return answer;
+        }
+    }
+}
---
+++ ch03/item28/ExtensionForIEnumerableCustomer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionForIEnumerableCustomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Name = "sasaki",
+                    Email = "somewhere@example.com",
+                    LastOrderDate = DateTime.Now - TimeSpan.FromDays(50)
+                },
+                new Customer
+                {
+                    Name = "sophia",
+                    Email = "sophia@example.com",
+                    LastOrderDate = DateTime.Now - TimeSpan.FromDays(40)
+                },
+                new Customer
+                {
+                    Name = "xyz",
+                    Email = "xyz@example.com",
+                    LastOrderDate = DateTime.Now - TimeSpan.FromDays(30)
+                },
+                new Customer
+                {
+                    Name = "abc",
+                    Email = "abc@example.com",
+                    LastOrderDate = DateTime.Now - TimeSpan.FromDays(20)
+                },
+                new Customer
+                {
+                    Name = "someone",
+                    Email = "someone@example.com",
+                    LastOrderDate = DateTime.Now - TimeSpan.FromDays(10)
+                }
+            };
+
+            Console.WriteLine("Test for SendEmailCoupnes(for all Cusomters):");
+            customers.SendEmailCoupons(new Coupon { Message = "offer for all customers" });
+
+            Console.WriteLine("Test for SendEmailCoupnes(for lost prospects):");
+            customers.LostProspects().SendEmailCoupons(
+                new Coupon { Message = "offer for lost prospects" });
+        }
+    }
+}
