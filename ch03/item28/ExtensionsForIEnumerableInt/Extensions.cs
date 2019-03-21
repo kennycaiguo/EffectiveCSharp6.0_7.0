d9c65a4122150c8cf24cf241f5cb1074ed570c3e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-21 10:11:32 +0900

    item28: ExtensionsForIEnumerableInt

---
+++ ch03/item28/ExtensionsForIEnumerableInt/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsForIEnumerableInt
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Generate<T>(int count, Func<int, T> fn)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return fn(i);
+            }
+        }
+    }
+}
---
+++ ch03/item28/ExtensionsForIEnumerableInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsForIEnumerableInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seq = Extensions.Generate(100, (i) => i * (100-i));
+
+            double average = seq.Average();
+            int max = seq.Max();
+            int min = seq.Min();
+            int sum = seq.Sum();
+
+            Console.WriteLine($"Average: {average}, Max: {max}, Min: {min}, Sum: {sum}");
+        }
+    }
+}
