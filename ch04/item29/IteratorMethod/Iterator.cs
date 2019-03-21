bb13481bbbb498398efa425ccc685d209291d1a3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-21 21:43:17 +0900

    item29: iteratorMethod

---
+++ ch04/item29/IteratorMethod/Iterator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorMethod
+{
+    public static class Iterator
+    {
+        public static IEnumerable<char> GenerateAlphabet()
+        {
+            var letter = 'a';
+            while (letter <= 'z')
+            {
+                yield return letter;
+                letter++;
+            }
+        }
+
+        public class EmbeddedIterator : IEnumerable<char>
+        {
+            public IEnumerator<char> GetEnumerator() => new LetterEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => new LetterEnumerator();
+
+            public static IEnumerable<char> GenerateAlphabet() => new EmbeddedIterator();
+
+            private class LetterEnumerator : IEnumerator<char>
+            {
+                private char letter = (char)('a' - 1);
+
+                public bool MoveNext()
+                {
+                    letter++;
+                    return letter <= 'z';
+                }
+
+                public char Current => letter;
+
+                object IEnumerator.Current => letter;
+
+                public void Reset() => letter = (char)('a' - 1);
+
+                public void Dispose() { }
+            }
+        }
+    }
+}
---
+++ ch04/item29/IteratorMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorMethod
+{
+    using static Iterator;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in GenerateAlphabet())
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            foreach (var i in EmbeddedIterator.GenerateAlphabet())
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+
+            var enumerable = EmbeddedIterator.GenerateAlphabet();
+            var enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.Write("{0} ", enumerator.Current);
+            }
+            enumerator.Dispose();
+            Console.WriteLine();
+        }
+    }
+}
