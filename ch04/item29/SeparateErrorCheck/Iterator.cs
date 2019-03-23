13f58d1f43f713e595bc015fd565128616f2271b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-24 08:25:49 +0900

    item29: SeparateErrorCheck

---
+++ ch04/item29/SeparateErrorCheck/Iterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateErrorCheck
+{
+    public static class Iterator
+    {
+        public static IEnumerable<char>
+            GenerateAlphabetSubset(char first, char last)
+        {
+            if (first < 'a')
+                throw new ArgumentException(
+                    "1文字目はa以降にする必要があります", nameof(first));
+            if (first > 'z')
+                throw new ArgumentException(
+                    "1文字目はz以前にする必要があります", nameof(first));
+            if (last < first)
+                throw new ArgumentException(
+                    "最後の文字は1文字目より後方になければいけません", nameof(last));
+            if (last > 'z')
+                throw new ArgumentException(
+                    "最後の文字はz以前にする必要があります", nameof(last));
+
+            return GenerateAlphabetSubsetImpl(first, last);
+        }
+
+        private static IEnumerable<char> GenerateAlphabetSubsetImpl(
+            char first, char last)
+        {
+            var letter = first;
+            while (letter <= last)
+            {
+                yield return letter;
+                letter++;
+            }
+        }
+    }
+}
---
+++ ch04/item29/SeparateErrorCheck/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateErrorCheck
+{
+    using static Iterator;
+
+    class Program
+    {
+        static void PrintChars(char first, char last)
+        {
+            Console.WriteLine($"\nPrintChars(first={(int)first}, last={(int)last}):\n");
+
+            IEnumerable<char> enemerable;
+            try
+            {
+                Console.WriteLine($"try call GenerateAlphabetSubset({(int)first},{(int)last})");
+                enemerable = GenerateAlphabetSubset(first, last);
+                try
+                {
+                    Console.WriteLine("try print chars");
+                    foreach (var c in enemerable)
+                    {
+                        Console.Write(c);
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintChars('a', 'z');
+            PrintChars('\t', 'z');
+            PrintChars((char)127, 'z');
+            PrintChars('b', 'a');
+            PrintChars('a', (char)127);
+        }
+    }
+}
