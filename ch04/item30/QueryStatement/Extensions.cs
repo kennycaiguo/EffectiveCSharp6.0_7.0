b0b4377ec6933e32061613a726b790d7f464c3d9
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-24 21:01:55 +0900

    item30: QueryStatement

---
+++ ch04/item30/QueryStatement/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryStatement
+{
+    public static class Extensions
+    {
+        public static void ForAll<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach (T item in sequence)
+                action(item);
+        }
+    }
+}
---
+++ ch04/item30/QueryStatement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = (from n in Enumerable.Range(0, 100)
+                       select n * n).ToArray();
+
+            foo.ForAll((n) => Console.WriteLine(n.ToString()));
+        }
+    }
+}
