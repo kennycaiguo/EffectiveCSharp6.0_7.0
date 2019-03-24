6683500afcdecb89c034630e9e947869918e2335
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-24 19:48:59 +0900

    item30: LoopStatement

---
+++ ch04/item30/LoopStatement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new int[100];
+
+            for (var num = 0; num < foo.Length; num++)
+                foo[num] = num * num;
+
+            foreach (int i in foo)
+                Console.WriteLine(i.ToString());
+        }
+    }
+}
