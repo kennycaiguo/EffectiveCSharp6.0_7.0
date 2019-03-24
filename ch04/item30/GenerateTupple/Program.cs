5eac15ccf44fe3ab6f99ac126c56b5fd573b07c8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-24 21:30:36 +0900

    item30: GenerateTuple

---
+++ ch04/item30/GenerateTupple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateTupple
+{
+    class Program
+    {
+        private static IEnumerable<Tuple<int, int>> ProduceIndices()
+        {
+            for (var x = 0; x < 100; x++)
+                for (var y = 0; y < 100; y++)
+                    yield return Tuple.Create(x, y);
+        }
+
+        private static IEnumerable<Tuple<int, int>> QueryIndices()
+        {
+            return from x in Enumerable.Range(0, 100)
+                    from y in Enumerable.Range(0, 100)
+                    select Tuple.Create(x, y);
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var tupple in ProduceIndices())
+                Console.WriteLine(tupple);
+            Console.WriteLine("generated tuples with nested loop:");
+
+            Console.Write("enter return-key to continue: ");
+            Console.ReadLine();
+
+            foreach (var tupple in QueryIndices())
+                Console.WriteLine(tupple);
+            Console.WriteLine("generated tuples with nested query:");
+        }
+    }
+}
