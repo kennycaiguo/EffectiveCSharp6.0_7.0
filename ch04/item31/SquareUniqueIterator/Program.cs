935304f0034ff375bbbfe880e571c0b0efb5b256
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-26 21:07:58 +0900

    iem31: SquareUniqueIterator

---
+++ ch04/item31/SquareUniqueIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace SquareUniqueIterator
+{
+    class Program
+    {
+        public static IEnumerable<T> Unique<T>(IEnumerable<T> sequence)
+        {
+            var uniqueVals = new HashSet<T>();
+            foreach (T item in sequence)
+            {
+                if (!uniqueVals.Contains(item))
+                {
+                    uniqueVals.Add(item);
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<int> Square(IEnumerable<int> nums)
+        {
+            foreach (var num in nums)
+                yield return num * num;
+        }
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 3, 4, 5, 7, 3, 2, 7, 8, 0, 3, 1 };
+            foreach (var num in Square(Unique(nums)))
+                WriteLine(num);
+        }
+    }
+}
