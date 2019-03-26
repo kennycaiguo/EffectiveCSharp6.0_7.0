219f17b07f07fc026eaca6e2d55d643839da3381
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-26 19:57:39 +0900

    item31: UniqueLoop

---
+++ ch04/item31/UniqueLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 2019.03.26 add
+using static System.Console;
+
+namespace UniqueLoop
+{
+    class Program
+    {
+        public static void Unique(IEnumerable<int> nums)
+        {
+            var uniqueVals = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (!uniqueVals.Contains(num))
+                {
+                    uniqueVals.Add(num);
+                    WriteLine(num);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 3, 4, 5, 7, 3, 2, 7, 8, 0, 3, 1 };
+            Unique(nums);
+        }
+    }
+}
