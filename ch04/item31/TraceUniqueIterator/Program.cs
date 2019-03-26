a5837eded61efbdb895b6e112e8d56e545abbb00
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-26 20:17:19 +0900

    item31: TraceUniqueIterator

---
+++ ch04/item31/TraceUniqueIterator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace TraceUniqueIterator
+{
+    class Program
+    {
+        public static IEnumerable<int> Unique2(IEnumerable<int> nums)
+        {
+            var uniqueVals = new HashSet<int>();
+            WriteLine("\tUniqueを実行中");
+            foreach (var num in nums)
+            {
+                WriteLine($"\t{num} を評価中");
+                if (!uniqueVals.Contains(num))
+                {
+                    WriteLine($"\t{num} を追加中");
+                    uniqueVals.Add(num);
+                    yield return num;
+                    WriteLine($"\tyield return 後に再実行中");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 0, 3, 4, 5, 7, 3, 2, 7, 8, 0, 3, 1 };
+            foreach (var num in Unique2(nums))
+                WriteLine(num);
+        }
+    }
+}
