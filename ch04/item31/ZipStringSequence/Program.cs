1c7b0acf695e996d87c44b8d94823b499f45e348
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-26 21:20:38 +0900

    item31: ZipStringSequence

---
+++ ch04/item31/ZipStringSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ZipStringSequence
+{
+    class Program
+    {
+        public static IEnumerable<string> Zip(IEnumerable<string> first,
+            IEnumerable<string> second)
+        {
+            using (var firstSequence = first.GetEnumerator())
+            {
+                using (var secondeSequence = second.GetEnumerator())
+                {
+                    while (firstSequence.MoveNext() &&
+                        secondeSequence.MoveNext())
+                    {
+                        yield return string.Format("{0} {1}",
+                            firstSequence.Current,
+                            secondeSequence.Current);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var first = new string[] { "i", "ii", "iii", "iv", "v", "vi" };
+            var second = new string[] { "one", "two", "three", "four", "five" };
+            foreach (var pair in Zip(first, second))
+                WriteLine(pair);
+        }
+    }
+}
