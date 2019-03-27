487a3905ca7717c8e18827b3a284896921781bc0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-27 20:59:22 +0900

    item32: ListManipulate

---
+++ ch04/item32/ListManipulate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ListManipulate
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInts = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            myInts.RemoveAll(collectionMember => collectionMember == 5);
+            myInts.ForEach(collectionMember => WriteLine(collectionMember));
+        }
+    }
+}
