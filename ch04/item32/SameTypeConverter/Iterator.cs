d9712e0d5567e92f3f3ffe1cbc3b9dbd0b23dc8d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-28 06:44:13 +0900

    item32: SameTypeConverter

---
+++ ch04/item32/SameTypeConverter/Iterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameTypeConverter
+{
+    public static class Iterator
+    {
+        public static IEnumerable<T> Select<T>(
+            IEnumerable<T> sequence, Func<T, T> method)
+        {
+            // sequenceとmethodのnullチェックは省略
+            foreach (T element in sequence)
+                yield return method(element);
+        }
+    }
+}
---
+++ ch04/item32/SameTypeConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace SameTypeConverter
+{
+    using static Iterator;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInts = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int i in Select(myInts, value => value * value))
+                WriteLine(i);
+        }
+    }
+}
