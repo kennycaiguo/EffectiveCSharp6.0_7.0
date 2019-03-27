a7d691016f17a40f6b910a147cd6202f79d5cc5c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-28 07:24:30 +0900

    item33: CreateIntList: test for BindingList

---
+++ ch04/item33/CreateIntList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateIntList
+{
+    class Program
+    {
+        static IList<int> CreateSequence(int numberOfElements,
+            int startAt, int stepBy)
+        {
+            var collection =
+                new List<int>(numberOfElements);
+            for (int i = 0; i < numberOfElements; i++)
+                collection.Add(startAt + i * stepBy);
+            return collection;
+        }
+
+        static void TestBindingList()
+        {
+            Console.WriteLine("TestBindingList():");
+
+            var list = CreateSequence(100, 0, 5);
+            var data1 = new BindingList<int>(list);
+            var data2 = new BindingList<int>(list);
+
+            data1[5] = 10000;
+            foreach (var i in data2)
+                Console.Write("{0} ", i);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            TestBindingList();
+        }
+    }
+}
