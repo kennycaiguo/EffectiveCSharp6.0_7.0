9954e18636065a61905660c46ab14964e6261fa7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-27 21:05:07 +0900

    item32: ListManipulate: change test input sequence

--- ch04/item32/ListManipulate/Program.cs
+++ ch04/item32/ListManipulate/Program.cs
@@ -13,7 +13,7 @@ namespace ListManipulate
     {
         static void Main(string[] args)
         {
-            var myInts = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var myInts = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
             myInts.RemoveAll(collectionMember => collectionMember == 5);
             myInts.ForEach(collectionMember => WriteLine(collectionMember));
         }
