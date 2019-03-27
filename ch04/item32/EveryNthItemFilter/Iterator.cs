72eb2ea1877b018cef49ff2eed71a9fdba3e6fff
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-27 21:37:05 +0900

    item32: EveryNthItemFilter

---
+++ ch04/item32/EveryNthItemFilter/Iterator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveryNthItemFilter
+{
+    public static class Iterator
+    {
+        public static IEnumerable<T> EveryNthItem<T>(
+            IEnumerable<T> sequence, int period)
+        {
+            var count = 0;
+            foreach (T item in sequence)
+                if (++count % period == 0)
+                    yield return item;
+        }
+    }
+}
---
+++ ch04/item32/EveryNthItemFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace EveryNthItemFilter
+{
+    using static Iterator;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ints = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var filtered = EveryNthItem(ints, 3);
+            foreach (var item in filtered)
+                WriteLine(item);
+        }
+    }
+}
