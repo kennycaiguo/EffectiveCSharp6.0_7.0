7d8f1cfd99e52d3482636a0965f6aeac860e7b69
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-28 06:54:26 +0900

    item32: DifferenctTypeConverter

---
+++ ch04/item32/DifferentTypeConverter/Iterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentTypeConverter
+{
+    public static class Iterator
+    {
+        public static IEnumerable<Tout> Select<Tin, Tout>(
+            IEnumerable<Tin> sequence, Func<Tin, Tout> method)
+        {
+            // sequenceとmethodのnullチェックは省略
+            foreach (Tin element in sequence)
+                yield return method(element);
+        }
+    }
+}
---
+++ ch04/item32/DifferentTypeConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace DifferentTypeConverter
+{
+    using static Iterator;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInts = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            WriteLine("convert int to string:");
+            foreach (string s in Select(myInts, value => value.ToString()))
+                WriteLine(s);
+
+            WriteLine("convert int to double:");
+            foreach (double d in Select(myInts, value => Math.Sqrt(value)))
+                WriteLine(d);
+
+        }
+    }
+}
