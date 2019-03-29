6e92e8fb0f9f90b2216309ab19844cdfabf2b721
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-30 07:48:23 +0900

    item34: ListRemoveAll

---
+++ ch04/item34/ListRemoveAll/MyList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListRemoveAll
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        List<T> list;
+        public MyList(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public void RemoveAll(IPredicate<T> match)
+        {
+            list.RemoveAll(match.Match);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+    }
+}
---
+++ ch04/item34/ListRemoveAll/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListRemoveAll
+{
+    class Program
+    {
+        static void FuncArgVersion()
+        {
+            Console.WriteLine("FuncArgVersion():");
+
+            var ints = new List<int>
+                { 0, 11, 22, 32, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999 };
+            ints.RemoveAll(i => i < 100);
+            foreach (var i in ints)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+        }
+
+        public class MyPredicate : IPredicate<int>
+        {
+            public bool Match(int target) => target < 100;
+        }
+
+        static void InterfaceVersion()
+        {
+            Console.WriteLine("InterfaceVersion():");
+
+            var ints = new List<int>
+                { 0, 11, 22, 32, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999 };
+            var myInts = new MyList<int>(ints);
+            var myPred = new MyPredicate();
+            myInts.RemoveAll(myPred);
+            foreach (var i in myInts)
+                Console.Write($"{i} ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            FuncArgVersion();
+            InterfaceVersion();
+        }
+    }
+}
