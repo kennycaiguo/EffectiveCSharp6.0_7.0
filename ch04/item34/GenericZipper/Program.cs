fc78d7a9be2970fca51e8f546a872c3074b06814
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-30 08:10:50 +0900

    item34: GenericZipper

---
+++ ch04/item34/GenericZipper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericZipper
+{
+    class Program
+    {
+        static void TestStringZip()
+        {
+            Console.WriteLine("TestStringZip():");
+
+            var first = new string[] { "1", "2", "3", "4", "5", "6" };
+            var second = new string[] { "i", "ii", "iii", "iv", "v", "vi" };
+            var zip = Util.Zip(first, second);
+            foreach (var elem in zip)
+                Console.WriteLine(elem);
+        }
+
+        static void TestGenericZip()
+        {
+            Console.WriteLine("TestGenericZip():");
+
+            var first = new int[] { 1, 2, 3, 4, 5, 6 };
+            var second = new string[] { "i", "ii", "iii", "iv", "v", "vi" };
+            var zip = Util.Zip(first, second, 
+                (one, two) => string.Format($"{one} {two}"));
+            foreach (var elem in zip)
+                Console.WriteLine(elem);
+        }
+
+        static void Main(string[] args)
+        {
+            TestStringZip();
+            TestGenericZip();
+        }
+    }
+}
---
+++ ch04/item34/GenericZipper/Util.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericZipper
+{
+    public static class Util
+    {
+        public static IEnumerable<string> Zip(
+            IEnumerable<string> first,
+            IEnumerable<string> second)
+        {
+            using (var firstSequence = first.GetEnumerator())
+            {
+                using (var secondSequence = second.GetEnumerator())
+                {
+                    while (firstSequence.MoveNext() &&
+                        secondSequence.MoveNext())
+                        yield return string.Format("{0} {1}",
+                            firstSequence.Current,
+                            secondSequence.Current);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> Zip<T1, T2, TResult>(
+            IEnumerable<T1> first,
+            IEnumerable<T2> second,
+            Func<T1, T2, TResult> zipper)
+        {
+            using (var firstSequence = first.GetEnumerator())
+            {
+                using (var secondSequence = second.GetEnumerator())
+                {
+                    while (firstSequence.MoveNext() &&
+                        secondSequence.MoveNext())
+                        yield return zipper(firstSequence.Current,
+                            secondSequence.Current);
+                }
+            }
+        }
+    }
+}
