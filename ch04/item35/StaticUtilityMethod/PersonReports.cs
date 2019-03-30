91056abb6423fb17d2534c8c829e04e37c3fcb4b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-31 07:28:11 +0900

    item35: StaticUtilityMethod

---
+++ ch04/item35/StaticUtilityMethod/PersonReports.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using PersonLib;
+
+namespace StaticUtilityMethod
+{
+    public static class PersonReports
+    {
+        public static string FormatAsText(Person target) =>
+            $"{target.LastName,20}, {target.FirstName,15}";
+        public static string FormatAsXML(Person target) =>
+            new XElement("Person",
+                new XElement("LastName", target.LastName),
+                new XElement("FirstName", target.FirstName)
+            ).ToString();
+    
+    }
+}
---
+++ ch04/item35/StaticUtilityMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PersonLib;
+namespace StaticUtilityMethod
+{
+    using static PersonReports;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> somePresidents =
+                new List<Person>
+                {
+                    new Person
+                    {
+                        FirstName = "George",
+                        LastName = "Washington"
+                    },
+                    new Person
+                    {
+                        FirstName = "Thomas",
+                        LastName = "Jefferson"
+                    },
+                    new Person
+                    {
+                        FirstName = "Abe",
+                        LastName = "Lincoln"
+                    }
+                };
+            foreach (Person p in somePresidents)
+            {
+                Console.WriteLine(FormatAsText(p));
+                Console.WriteLine(FormatAsXML(p));
+            }
+        }
+    }
+}
