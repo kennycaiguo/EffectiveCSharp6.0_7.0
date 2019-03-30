f6629dbbcbd6d9ccf591a6b0ebd93fefdacc8f84
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-30 13:39:24 +0900

    item34: GenericSum

---
+++ ch04/item34/GenericSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSum
+{
+    using static Util;
+
+    class Program
+    {
+        static void TestIntSum()
+        {
+            Console.WriteLine("TestIntSum():");
+            var sequence = Enumerable.Range(1, 100);
+            var total = 0;
+            total = Sum(sequence);
+            Console.WriteLine($"total: {total}");
+        }
+
+        static void TestGenericSum()
+        {
+            Console.WriteLine("TestGenericSum():");
+            var sequence = Enumerable.Range(1, 100);
+            var total = 0;
+            total = Sum(sequence, total, (sum, num) => sum + num);
+            Console.WriteLine($"total: {total}");
+        }
+
+        static void Main(string[] args)
+        {
+            TestIntSum();
+            TestGenericSum();
+        }
+    }
+}
---
+++ ch04/item34/GenericSum/Util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSum
+{
+    public static class Util
+    {
+        public static int Sum(IEnumerable<int> nums)
+        {
+            var total = 0;
+            foreach (int num in nums)
+            {
+                total += num;
+            }
+            return total;
+        }
+
+        public static T Sum<T>(IEnumerable<T> sequence, T total,
+            Func<T, T, T> accumulator)
+        {
+            foreach (T item in sequence)
+            {
+                total = accumulator(total, item);
+            }
+            return total;
+        }
+    }
+}
