94d0803cc32ea3d5222e5c9107e2a51f5f1b5f5a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-30 18:10:39 +0900

    item34: GenericFold

---
+++ ch04/item34/GenericFold/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFold
+{
+    using static Util;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var peeps = new List<Employee>()
+            {
+                new Employee { Name = "Sasaki", Salary = 62.5M },
+                new Employee { Name = "Sophia", Salary = 25M },
+                new Employee {Name = "Lisa", Salary = 30M}
+            };
+
+            var totalSalary = Fold(peeps, 0M, (person, sum) => sum + person.Salary);
+            Console.WriteLine($"totalSalary: {totalSalary}");
+        }
+    }
+}
---
+++ ch04/item34/GenericFold/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFold
+{
+    public static class Util
+    {
+        public static TResult Fold<T, TResult>(
+            IEnumerable<T> sequence,
+            TResult total,
+            Func<T, TResult, TResult> accumulator)
+        {
+            foreach (T item in sequence)
+            {
+                total = accumulator(item, total);
+            }
+            return total;
+        }
+    }
+}
