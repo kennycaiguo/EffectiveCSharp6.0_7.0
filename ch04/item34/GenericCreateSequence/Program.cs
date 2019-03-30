66a3a6957e8fc801fa028503cbc1a5b9e49edc2c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-30 13:23:12 +0900

    item34: GenericCreateSequence

---
+++ ch04/item34/GenericCreateSequence/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCreateSequence
+{
+    class Program
+    {
+        static void TestCreateIntSequence()
+        {
+            Console.WriteLine("TestCreateIntSequence():");
+            foreach (var i in Util.CreateSequence(20, 0, 5))
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
+        }
+
+        static void TestCreateGenericSequence()
+        {
+            Console.WriteLine("TestCreateGenericSequence():");
+
+            var startAt = 0;
+            var nextValue = 5;
+            foreach (var i in Util.CreateSequence(20, () => startAt += nextValue))
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
+        }
+
+        static void TestCreateGenericSequenceImproved()
+        {
+            Console.WriteLine("TestCreateGenericSequenceImproved():");
+
+            var startAt = 0;
+            var nextValue = 5;
+            foreach (var i in Util.CreateSequence(20, (i) => startAt + i * nextValue))
+                Console.Write($"{i} ");
+            Console.WriteLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            TestCreateIntSequence();
+            TestCreateGenericSequence();
+            TestCreateGenericSequenceImproved();
+        }
+    }
+}
---
+++ ch04/item34/GenericCreateSequence/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCreateSequence
+{
+    public static class Util
+    {
+        public static IEnumerable<int> CreateSequence(int numberOfElements,
+            int startAt, int stepBy)
+        {
+            for (var i = 0; i < numberOfElements; i++)
+                yield return startAt + i * stepBy;
+        }
+
+        public static IEnumerable<T> CreateSequence<T>(
+            int numberOfElements,
+            Func<T> generator)
+        {
+            for (var i = 0; i < numberOfElements; i++)
+                yield return generator();
+        }
+
+        public static IEnumerable<T> CreateSequence<T>(
+            int numberOfElements,
+            Func<int, T> generator)
+        {
+            for (var i = 0; i < numberOfElements; i++)
+                yield return generator(i);
+        }
+    }
+}
