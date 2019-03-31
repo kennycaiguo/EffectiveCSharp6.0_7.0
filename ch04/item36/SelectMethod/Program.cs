1c022b09ac007d992907ae90be22ff81dbe76c96
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-31 20:33:33 +0900

    item36: SelectMethod: add value-conversion-select

--- ch04/item36/SelectMethod/Program.cs
+++ ch04/item36/SelectMethod/Program.cs
@@ -32,9 +32,37 @@ namespace SelectMethod
             Console.WriteLine();
         }
 
+        static void TestValueConversionSelect()
+        {
+            Console.WriteLine("TestValueConversionSelect():");
+
+            var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var allNumbers = from n in numbers
+                             where n < 5
+                             select n * n;
+            foreach (var n in allNumbers)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
+            allNumbers = numbers.Where(n => n < 5).Select(n => n * n);
+            foreach (var n in allNumbers)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
+            var tr_numbers = new LinqTracer<int>(numbers);
+            var tr_allNumbers = from n in tr_numbers
+                                where n < 5
+                                select n * n;
+            foreach (var n in tr_allNumbers.AsEnumerable())
+                Console.Write($"{n} ");
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             TestSimpleSelect();
+            TestValueConversionSelect();
         }
     }
 }
