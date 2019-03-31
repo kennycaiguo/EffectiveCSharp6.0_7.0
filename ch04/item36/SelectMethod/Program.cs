e67d71b96956b28b4a073518531eaca59a6d8174
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-03-31 20:39:37 +0900

    item36: SelectMethod: add type-conversion-select

--- ch04/item36/SelectMethod/Program.cs
+++ ch04/item36/SelectMethod/Program.cs
@@ -34,7 +34,7 @@ namespace SelectMethod
 
         static void TestValueConversionSelect()
         {
-            Console.WriteLine("TestValueConversionSelect():");
+            Console.WriteLine("\nTestValueConversionSelect():");
 
             var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
@@ -59,10 +59,36 @@ namespace SelectMethod
             Console.WriteLine();
         }
 
+        static void TestTypeConversionSelect()
+        {
+            Console.WriteLine("\nTestTypeConversionSelect():");
+
+            var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var allNumbers = from n in numbers
+                             select new { Number = n, Square = n * n };
+            foreach (var n in allNumbers)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
+            allNumbers = numbers.Select(n => new { Number = n, Square = n * n });
+            foreach (var n in allNumbers)
+                Console.Write($"{n} ");
+            Console.WriteLine();
+
+            var tr_numbers = new LinqTracer<int>(numbers);
+            var tr_allNumbers = from n in tr_numbers
+                                select new { Number = n, Square = n * n };
+            foreach (var n in tr_allNumbers.AsEnumerable())
+                Console.Write($"{n} ");
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             TestSimpleSelect();
             TestValueConversionSelect();
+            TestTypeConversionSelect();
         }
     }
 }
