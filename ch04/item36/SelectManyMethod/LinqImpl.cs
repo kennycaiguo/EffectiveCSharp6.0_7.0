062cf20646c4bdc8d7cbcb4d9a7a3f7c56c09108
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-06 01:57:47 +0900

    item36: SelectManyMethod: add Test_SelectManyImplement()

---
+++ ch04/item36/SelectManyMethod/LinqImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectManyMethod
+{
+    public static class LinqImpl
+    {
+        public static IEnumerable<TOutput> MySelectMany<T1, T2, TOutput>(
+            this IEnumerable<T1> src,
+            Func<T1, IEnumerable<T2>> inputSelector,
+            Func<T1, T2, TOutput> resultSelector)
+        {
+            foreach (T1 first in src)
+            {
+                foreach (T2 second in inputSelector(first))
+                    yield return resultSelector(first, second);
+            }
+        }
+    }
+}
