ffb0442dba4c4f293d113de2c27e38c8569f7798
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-10 16:56:33 +0900

    item37: InfiniteSequence_Where

---
+++ ch04/item37/InfiniteSequence_Where/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteSequence_Where
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answers = from number in AllNumbers()
+                          where number < 10
+                          select number;
+            foreach (var num in answers)
+                Console.WriteLine(num);
+        }
+
+        static IEnumerable<int> AllNumbers()
+        {
+            var number = 0;
+            while (number < int.MaxValue)
+            {
+                yield return number++;
+            }
+        }
+    }
+}
