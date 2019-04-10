2438482ea6464bc94421ca15b716f52dde882030
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-10 16:48:42 +0900

    item37: InfiniteSequence_Take

---
+++ ch04/item37/InfiniteSequence_Take/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteSequence_Take
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answers = from number in AllNumbers()
+                          select number;
+            var smallNumbers = answers.Take(10);
+            foreach (var num in smallNumbers)
+                Console.WriteLine(num);
+        }
+
+        static IEnumerable<int> AllNumbers()
+        {
+            var number = 0;
+            while (number < int.MaxValue)
+            {
+                yield return number++;
+            }
+        }
+    }
+}
