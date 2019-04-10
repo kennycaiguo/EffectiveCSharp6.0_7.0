864eb25a05be3998c8cf7acea587892b58efefeb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-10 17:44:56 +0900

    item37: SortFilteredData

---
+++ ch04/item37/SortFilteredData/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortFilteredData
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int UnitsInStock { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ Name={Name}, UnitsInStock={UnitsInStock} }}";
+        }
+    }
+}
---
+++ ch04/item37/SortFilteredData/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortFilteredData
+{
+    class Program
+    {
+        static IEnumerable<Product> MakeProducts()
+        {
+            var products = new List<Product>();
+            for (int i = 0; i < 1000000; i++)
+            {
+                int units;
+                if ((i % 123456) == 0) units = (i % 100) + 100;
+                else units = i % 100;
+
+                Product p = new Product { Name = $"product#{i}", UnitsInStock = units };
+                products.Add(p);
+            }
+            return products;
+        }
+
+        static void Main(string[] args)
+        {
+            var products = MakeProducts();
+
+            var sortedProductsSlow =
+                from p in products
+                orderby p.UnitsInStock descending
+                where p.UnitsInStock > 100
+                select p;
+
+            var sortedProductsFast =
+                from p in products
+                where p.UnitsInStock > 100
+                orderby p.UnitsInStock descending
+                select p;
+
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            foreach (var p in sortedProductsSlow)
+                Console.WriteLine(p);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            Console.WriteLine($"sorted products slow: {ts}");
+
+            sw.Restart();
+            foreach (var p in sortedProductsFast)
+                Console.WriteLine(p);
+            sw.Stop();
+            ts = sw.Elapsed;
+            Console.WriteLine($"sorted products fast: {ts}");
+        }
+    }
+}
