dfe649ababd78995db06522b4e2d3d33d67f94ba
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-13 19:31:35 +0900

    item39: ExceptionInSequence

---
+++ ch04/item39/ExceptionInSequence/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionInSequence
+{
+    class Program
+    {
+        static List<Employee> FindAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee {
+                EmployeeID = 10001,
+                Classification = Classification.Salary,
+                YearsOfService = 21,
+                MonthlySalary = 56.7M }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10002,
+                Classification = Classification.Retired,
+                YearsOfService = 11,
+                MonthlySalary = 0 }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10003,
+                Classification = Classification.Manager,
+                YearsOfService = 23,
+                MonthlySalary = 78.9M
+            }
+            );
+            return employees;
+        }
+
+        static void Main(string[] args)
+        {
+            var allEmployees = FindAllEmployees();
+            try
+            {
+                allEmployees.ForEach(e => {
+                    if (e.MonthlySalary == 0)
+                        throw new ArithmeticException("singular value");
+                    else e.MonthlySalary *= 1.05M;
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            foreach (var employee in allEmployees)
+                Console.WriteLine(employee);
+        }
+    }
+}
