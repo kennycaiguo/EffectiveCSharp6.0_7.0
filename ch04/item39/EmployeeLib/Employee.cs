4668522ee4d09149b8c2ced49164aab0a5717a26
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-14 07:56:57 +0900

    item39: EmployeeLib: separate Employee class into library

---
+++ ch04/item39/EmployeeLib/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib
+{
+    public enum Classification
+    {
+        Salary,
+        Manager,
+        Retired
+    }
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public Classification Classification { get; set; }
+        public int YearsOfService { get; set; }
+        public decimal MonthlySalary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{ EmployeeID = {EmployeeID}, Classification = {Classification}, YearsOfService = {YearsOfService}, MonthlySalary = {MonthlySalary} }}";
+        }
+
+        public static List<Employee> FindAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee
+            {
+                EmployeeID = 10001,
+                Classification = Classification.Salary,
+                YearsOfService = 11,
+                MonthlySalary = 50.0M
+            }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10001,
+                Classification = Classification.Salary,
+                YearsOfService = 5,
+                MonthlySalary = 30.0M
+            }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10003,
+                Classification = Classification.Manager,
+                YearsOfService = 21,
+                MonthlySalary = 60.0M
+            }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10002,
+                Classification = Classification.Retired,
+                YearsOfService = 31,
+                MonthlySalary = decimal.MinValue
+            }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10003,
+                Classification = Classification.Manager,
+                YearsOfService = 23,
+                MonthlySalary = 70.0M
+            }
+            );
+            employees.Add(new Employee
+            {
+                EmployeeID = 10001,
+                Classification = Classification.Salary,
+                YearsOfService = 7,
+                MonthlySalary = 40.0M
+            }
+            );
+            return employees;
+        }
+    }
+}
--- ch04/item39/ExceptionInSequence/Program.cs
+++ ch04/item39/ExceptionInSequence/Program.cs
@@ -4,61 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EmployeeLib;
+using static EmployeeLib.Employee;
+
 namespace ExceptionInSequence
 {
     class Program
     {
-        static List<Employee> FindAllEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            employees.Add(new Employee {
-                EmployeeID = 10001,
-                Classification = Classification.Salary,
-                YearsOfService = 21,
-                MonthlySalary = 50.0M }
-            );
-            employees.Add(new Employee
-            {
-                EmployeeID = 10001,
-                Classification = Classification.Salary,
-                YearsOfService = 21,
-                MonthlySalary = 30.0M
-            }
-            );
-            employees.Add(new Employee
-            {
-                EmployeeID = 10003,
-                Classification = Classification.Manager,
-                YearsOfService = 23,
-                MonthlySalary = 60.0M
-            }
-            );
-            employees.Add(new Employee
-            {
-                EmployeeID = 10002,
-                Classification = Classification.Retired,
-                YearsOfService = 11,
-                MonthlySalary = decimal.MinValue }
-            );
-            employees.Add(new Employee
-            {
-                EmployeeID = 10003,
-                Classification = Classification.Manager,
-                YearsOfService = 23,
-                MonthlySalary = 70.0M
-            }
-            );
-            employees.Add(new Employee
-            {
-                EmployeeID = 10001,
-                Classification = Classification.Salary,
-                YearsOfService = 21,
-                MonthlySalary = 40.0M
-            }
-            );
-            return employees;
-        }
-
         static void Main(string[] args)
         {
             var allEmployees = FindAllEmployees();
