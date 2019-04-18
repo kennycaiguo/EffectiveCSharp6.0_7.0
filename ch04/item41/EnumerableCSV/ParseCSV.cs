db23603de2e4bcfc5fdf2b8f9b0adf81a392101b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-18 20:47:51 +0900

    item41: EnumerableCSV

---
+++ ch04/item41/EnumerableCSV/ParseCSV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableCSV
+{
+    public static class ParseCSV
+    {
+        public static IEnumerable<string> ReadLines(this TextReader reader)
+        {
+            var txt = reader.ReadLine();
+            while (txt != null)
+            {
+                yield return txt;
+                txt = reader.ReadLine();
+            }
+        }
+
+        public static int DefaultParse(this string input, int defaultValue)
+        {
+            int answer;
+            return (int.TryParse(input, out answer))
+                ? answer : defaultValue;
+        }
+
+        public static IEnumerable<IEnumerable<int>>
+            ReadNumbersFromStream(TextReader t)
+        {
+            var allLines = from line in t.ReadLines()
+                           select line.Split(',');
+            var matrixOfValues = from line in allLines
+                                 select from item in line
+                                        select item.DefaultParse(0);
+            return matrixOfValues;
+        }
+    }
+}
---
+++ ch04/item41/EnumerableCSV/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace EnumerableCSV
+{
+    using static ParseCSV;
+    class Program
+    {
+        private static readonly string DIR = @"C:\books\EffectiveCSharp6.0_7.0\ch04\item41\EnumerableCSV\data\";
+
+        static void TestParseCSV()
+        {
+            Console.WriteLine("TestParseCSV():");
+
+            var t = new StreamReader(File.OpenRead(DIR + "TestFile.txt"));
+            var rowOfNumbers = ReadNumbersFromStream(t);
+
+            foreach (var line in rowOfNumbers)
+            {
+                foreach (int num in line)
+                    Write("{0}, ", num);
+                WriteLine();
+            }
+        }
+
+        static void TestParseCSVwithAutoClose()
+        {
+            Console.WriteLine("TestParseCSVwithAutoClose():");
+
+            IEnumerable<IEnumerable<int>> rowOfNumbers;
+            using (TextReader t = new StreamReader(File.OpenRead(DIR + "TestFile.txt")))
+                rowOfNumbers = ReadNumbersFromStream(t);
+
+            foreach (var line in rowOfNumbers)
+            {
+                foreach (int num in line)
+                    Write("{0}, ", num);
+                WriteLine();
+            }
+        }
+
+        static void TestParseCSVwithinUsing()
+        {
+            Console.WriteLine("TestParseCSVwithinUsing():");
+
+            using (TextReader t = new StreamReader(File.OpenRead(DIR + "TestFile.txt")))
+            {
+                var rowOfNumbers = ReadNumbersFromStream(t);
+
+                foreach (var line in rowOfNumbers)
+                {
+                    foreach (int num in line)
+                        Write("{0}, ", num);
+                    WriteLine();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestParseCSV();
+            try
+            {
+                TestParseCSVwithAutoClose();
+            }
+            catch (Exception e)
+            {
+                WriteLine(e);
+            }
+            TestParseCSVwithinUsing();
+        }
+    }
+}
