68186ce547fb38ed7c8d584facf0a9da50b2bea1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-19 20:23:54 +0900

    item41: ParseCSVFile

---
+++ ch04/item41/ParseCSVFile/ParseCSV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseCSVFile
+{
+    public static class ParseCSV
+    {
+        public static IEnumerable<string> ParseFile(string path)
+        {
+            using (var r = new StreamReader(File.OpenRead(path)))
+            {
+                var txt = r.ReadLine();
+                while (txt != null)
+                {
+                    yield return txt;
+                    txt = r.ReadLine();
+                }
+            }
+        }
+
+        public static int DefaultParse(this string input, int defaultValue)
+        {
+            int answer;
+            return (int.TryParse(input, out answer))
+                ? answer : defaultValue;
+        }
+
+        public static IEnumerable<IEnumerable<int>>
+            ReadNumbersFromFile(string path)
+        {
+            var allLines = from line in ParseFile(path)
+                           select line.Split(',');
+            var matrixOfValues = from line in allLines
+                                 select from item in line
+                                        select item.DefaultParse(0);
+            return matrixOfValues;
+        }
+    }
+}
---
+++ ch04/item41/ParseCSVFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ParseCSVFile
+{
+    using static ParseCSV;
+    class Program
+    {
+        private static readonly string DIR = @"C:\books\EffectiveCSharp6.0_7.0\ch04\item41\ParseCSVFile\data\";
+
+        static void Main(string[] args)
+        {
+            var rowOfNumbers = ReadNumbersFromFile(DIR + "TestFile.txt");
+
+            foreach (var line in rowOfNumbers)
+            {
+                foreach (int num in line)
+                    Write("{0}, ", num);
+                WriteLine();
+            }
+
+        }
+    }
+}
