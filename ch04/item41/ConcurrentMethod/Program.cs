a523fa3ddc2fbeda7ba1264e8c198d48826b586c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-27 06:06:52 +0900

    item41: ConcurrentMethod

---
+++ ch04/item41/ConcurrentMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentMethod
+{
+    class Program
+    {
+        private static void SomeMethod(ref int i)
+        {
+            i = 123;
+        }
+
+        private static void DoSomethingInBackground()
+        {
+            var i = 0;
+            var thread = new Thread(delegate () { SomeMethod(ref i); });
+            thread.Start();
+
+            for (var j = 0; j < 10; j++)
+                Console.WriteLine($"i: {i}");
+        }
+
+        static void Main(string[] args)
+        {
+            DoSomethingInBackground();
+        }
+    }
+}
