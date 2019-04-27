cdcd3321ed152adf67bb9ad15e8f179364e49e5a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-04-27 09:35:54 +0900

    item43: SingleMethod

---
+++ ch04/item43/SingleMethod/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleMethod
+{
+    class Program
+    {
+        static void TestNotSingleElement()
+        {
+            Console.WriteLine("TestNotSingleElement():");
+
+            var somePeople = new List<Person>
+            {
+                new Person { FirstName = "Bill", LastName = "Gates" },
+                new Person { FirstName = "Bill", LastName = "Wagner" },
+                new Person { FirstName = "Bill", LastName = "Johnson" }
+            };
+
+            // シーケンス内に複数の該当データが存在するため
+            //　例外がスローされる
+            try
+            {
+                var answer = (from p in somePeople
+                              where p.FirstName == "Bill"
+                              select p).Single();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void TestZeroElement()
+        {
+            Console.WriteLine("TestZeroElement():");
+
+            var somePeople = new List<Person>
+            {
+                new Person { FirstName = "Bill", LastName = "Gates" },
+                new Person { FirstName = "Bill", LastName = "Wagner" },
+                new Person { FirstName = "Bill", LastName = "Johnson" }
+            };
+
+            // シーケンス内に複数の該当データが存在するため
+            //　例外がスローされる
+            try
+            {
+                var answer = (from p in somePeople
+                              where p.FirstName == "Larry"
+                              select p).Single();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestNotSingleElement();
+            TestZeroElement();
+        }
+    }
+}
